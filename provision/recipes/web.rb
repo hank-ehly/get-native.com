313c87ce4398f868c6a6aa2125ce4d4960bb3210
Hank Ehly <henry.ehly@gmail.com>
2016-10-16 18:14:29 +0900

    Move integration tests to their appropriate folders

--- provision/recipes/web.rb
+++ provision/recipes/web.rb
@@ -3,3 +3,12 @@
 # Recipe:: web
 #
 # Copyright (c) 2016 Hank Ehly, All Rights Reserved.
+
+include_recipe 'nodejs::nodejs_from_binary'
+include_recipe 'nodejs::npm'
+
+node[:nodejs][:global_npm_packages].each do |p|
+    nodejs_npm p do
+        user node[:user][:name]
+    end
+end
