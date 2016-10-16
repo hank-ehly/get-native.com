ae32e92b99ca82a6b658d6ddc19b4051715bc628
Hank Ehly <henry.ehly@gmail.com>
2016-10-16 20:08:13 +0900

    Add debug logging for bash script

--- provision/recipes/web.rb
+++ provision/recipes/web.rb
@@ -38,14 +38,13 @@ extract_path = Chef::Config[:file_cache_path] + '/apache2'
 
 bash 'mod_http2.so' do
     code <<-EOH
-        sudo apt-get source apache2
-        sudo mkdir -p #{extract_path} && cd #{extract_path}
-        sudo apt-get source apache2
-        sudo apt-get build-dep -y apache2
-        cd apache-2.4.18
-        sudo fakeroot debian/rules binary
-        sudo cp debian/apache2-bin/usr/lib/apache2/modules/mod_http2.so /usr/lib/apache2/modules/
-        sudo chown root:root /usr/lib/apache2/modules/mod_http2.so
+        sudo mkdir -p #{extract_path} && cd #{extract_path} && echo '[SUCCESS] cd extract_path'
+        sudo apt-get source apache2 && echo '[SUCCESS] sudo apt-get source apache2'
+        sudo apt-get build-dep -y apache2 && echo '[SUCCESS] sudo apt-get build-dep -y apache2'
+        cd apache-2.4.18 && echo '[SUCCESS] cd apache-2.4.18'
+        sudo fakeroot debian/rules binary && echo '[SUCCESS] sudo fakeroot debian/rules binary'
+        sudo cp debian/apache2-bin/usr/lib/apache2/modules/mod_http2.so /usr/lib/apache2/modules/ && echo '[SUCCESS] sudo cp debian...'
+        sudo chown root:root /usr/lib/apache2/modules/mod_http2.so && echo '[SUCCESS] sudo chown root:root ...'
     EOH
     not_if { ::File.exists?('/usr/lib/apache2/modules/mod_http2.so') }
 end
