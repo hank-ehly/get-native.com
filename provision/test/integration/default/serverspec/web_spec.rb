ef2ec80613b1d9e144bb2753712fcffd91b3c9a0
Hank Ehly <henry.ehly@gmail.com>
2016-10-16 18:10:16 +0900

    Remove test boiletplate

--- provision/test/integration/default/serverspec/web_spec.rb
+++ provision/test/integration/default/serverspec/web_spec.rb
@@ -1,9 +1,5 @@
 require 'spec_helper'
 
 describe 'get-native.com::web' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+
 end
