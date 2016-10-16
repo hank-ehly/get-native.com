54ae2775340c457aaec74519eb31ced6bcef31fb
Hank Ehly <henry.ehly@gmail.com>
2016-10-16 18:09:48 +0900

    Add chef db test script

--- provision/test/integration/default/serverspec/db_spec.rb
+++ provision/test/integration/default/serverspec/db_spec.rb
@@ -1,9 +1,9 @@
 require 'spec_helper'
 
 describe 'get-native.com::db' do
-  # Serverspec examples can be found at
-  # http://serverspec.org/resource_types.html
-  it 'does something' do
-    skip 'Replace this with meaningful tests'
-  end
+
+    describe service('mysql-get-native') do
+        it { should be_enabled }
+        it { should be_running }
+    end
 end
