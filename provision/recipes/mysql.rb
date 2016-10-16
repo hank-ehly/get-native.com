51a38226f41bde39c7460c9f6fb72192c1a99fea
Hank Ehly <henry.ehly@gmail.com>
2016-10-16 18:02:40 +0900

    Create simple mysql recipe

--- provision/recipes/mysql.rb
+++ provision/recipes/mysql.rb
@@ -3,3 +3,15 @@
 # Recipe:: mysql
 #
 # Copyright (c) 2016 Hank Ehly, All Rights Reserved.
+
+execute 'sudo apt-get update' do
+    action :nothing
+end
+
+mysql_service 'get-native' do
+    version '5.7'
+    initial_root_password 'root'
+    charset 'utf8'
+    notifies :run, 'execute[sudo apt-get update]', :immediately
+    action [:create, :start]
+end
