da58d54d1f9411893b6f9798207804c10f8c702d
Hank Ehly <henry.ehly@gmail.com>
2016-10-16 19:52:29 +0900

    Add http2 module building script and virtualhost template file

--- provision/recipes/web.rb
+++ provision/recipes/web.rb
@@ -26,9 +26,31 @@ include_recipe 'locale::default'
 
 include_recipe 'apache2::default'
 include_recipe 'apache2::mod_ssl'
-include_recipe 'apache2::mod_http2'
 include_recipe 'apache2::mod_deflate'
 include_recipe 'apache2::mod_rewrite'
+
+include_recipe 'apache2::mod_http2'
+package 'libnghttp2-dev'
+
+extract_path = Chef::Config[:file_cache_path] + '/apache2'
+
+bash 'mod_http2.so' do
+    code <<-EOH
+        sudo apt-get source apache2
+        sudo mkdir -p #{extract_path} && cd #{extract_path}
+        sudo apt-get source apache2
+        sudo apt-get build-dep apache2
+        cd apache-2.4.18
+        sudo fakeroot debian/rules binary
+        sudo cp debian/apache2-bin/usr/lib/apache2/modules/mod_http2.so
+    EOH
+    not_if { ::File.exists?('/usr/lib/apache2/modules/mod_http2.so') }
+end
+
+web_app 'get-native.com' do
+    template 'get-native.com.conf.erb'
+    server_name node['my_app']['hostname']
+end
 
 include_recipe 'nodejs::nodejs_from_binary'
 include_recipe 'nodejs::npm'
