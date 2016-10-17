89722228246b56e4fbfaccf1a4d3332f00c1fb0a
Hank Ehly <henry.ehly@gmail.com>
2016-10-18 07:55:14 +0900

    Add build-essential and update calls to db-server script

--- provision/recipes/mysql.rb
+++ provision/recipes/mysql.rb
@@ -4,13 +4,13 @@
 #
 # Copyright (c) 2016 Hank Ehly, All Rights Reserved.
 
-execute 'sudo apt-get update' do
-    action :nothing
-end
+execute 'sudo apt-get update'
+include_recipe 'build-essential::default'
 
 mysql_service 'get-native' do
     version '5.7'
     initial_root_password 'root'
+    bind_address '127.0.0.1'
     charset 'utf8'
     notifies :run, 'execute[sudo apt-get update]', :immediately
     action [:create, :start]
