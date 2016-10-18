402e97b31ca8bbfbcb33aa4b9f3dfbdbd7496555
Hank Ehly <henry.ehly@gmail.com>
2016-10-19 07:35:16 +0900

    Add knife-solo configuration above get-native.com recipe

--- provision/test/integration/db/serverspec/db_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-describe 'get-native.com::db' do
-
-    describe service('mysql-get-native') do
-        it { should be_enabled }
-        it { should be_running }
-    end
-end
