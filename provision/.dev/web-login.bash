31747c9a5a866b7f2a8cda555191985f3fa15a1c
Hank Ehly <henry.ehly@gmail.com>
2016-10-26 07:39:34 +0900

    Remove unused dev environment

--- provision/.dev/web-login.bash
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-ssh ubuntu@127.0.0.1 -p 2222 \
-                     -o "UserKnownHostsFile /dev/null" \
-                     -o "StrictHostKeyChecking no" \
-                     -o "PasswordAuthentication no" \
-                     -o "IdentitiesOnly yes" \
-                     -o "LogLevel FATAL" \
-                     -o "IdentityFile ~/Sites/get-native.com/provision/.dev/keys/dev.web.get-native.com"
