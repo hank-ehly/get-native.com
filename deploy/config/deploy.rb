830b60afce1335bf842540698e8c0615ac554a0a
Hank Ehly <henry.ehly@gmail.com>
2016-10-29 21:30:09 +0900

    Remove unused 'user' setting from deploy config

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -7,3 +7,7 @@ set :repo_url, 'git@github.com:hank-ehly/get-native.com.git'
 set :branch, 'master'
 set :deploy_to, "/var/www/#{fetch(:application).to_s}/#{fetch(:stage)}"
 set :scm, :git
+set :keep_releases, 3
+
+after 'deploy:finished', 'deploy:npm_install'
+after 'deploy:npm_install', 'deploy:build_prod'
--- deploy/config/deploy/staging.rb
+++ deploy/config/deploy/staging.rb
@@ -1,2 +1,3 @@
-set :keep_releases, 3
-server fetch(:staging_host), user: fetch(:staging_user), ssh_options: {forward_agent: false}, roles: %{web}
+server fetch(:staging_host),
+       ssh_options: {forward_agent: false},
+       roles: %{web}
