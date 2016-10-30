90b24c853baeeab95f8f393a0b61f4df401bb4fb
Hank Ehly <henry.ehly@gmail.com>
2016-10-30 14:48:34 +0900

    Remove ssh key location from staging config

--- deploy/config/deploy/staging.rb
+++ deploy/config/deploy/staging.rb
@@ -1,8 +1,7 @@
-server '54.199.146.132',
+server 'deploy.stg.web.get-native.com',
        user: 'get-native',
        roles: %{web},
        ssh_options: {
            forward_agent: false,
-           auth_methods: %w(publickey),
-           keys: %w(~/.ssh/get-native.com/stg.web.get-native.com)
+           auth_methods: %w(publickey)
        }
