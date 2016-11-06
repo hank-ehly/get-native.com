ec221296b5e6d850589f433d02df2e931d243b29
Hank Ehly <henry.ehly@gmail.com>
2016-11-06 10:46:50 +0900

    Purge the src/ directory removing anything not to be used in get-native

--- src/client/app/shared/toolbar/toolbar.component.ts
+++ src/client/app/shared/toolbar/toolbar.component.ts
@@ -1,14 +1,11 @@
 import { Component } from '@angular/core';
 
-/**
- * This class represents the toolbar component.
- */
 @Component({
-  moduleId: module.id,
-  selector: 'sd-toolbar',
-  templateUrl: 'toolbar.component.html',
-  styleUrls: ['toolbar.component.css']
+    moduleId: module.id,
+    selector: 'gn-toolbar',
+    templateUrl: 'toolbar.component.html',
+    styleUrls: ['toolbar.component.css']
 })
 
-export class ToolbarComponent {}
-
+export class ToolbarComponent {
+}
