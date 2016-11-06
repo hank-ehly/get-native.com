dce95979efc8a12d55fa4da813f8ee5e5bfb3419
Hank Ehly <henry.ehly@gmail.com>
2016-11-06 16:52:48 +0900

    Add footer component

---
+++ src/client/app/core/footer/footer.component.ts
@@ -0,0 +1,19 @@
+/**
+ * footer.component
+ * get-native.com
+ *
+ * Created by henryehly on 2016/11/06.
+ */
+
+import { Component } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'gn-footer',
+    templateUrl: 'footer.component.html',
+    styleUrls: ['footer.component.css']
+})
+
+export class FooterComponent {
+
+}
