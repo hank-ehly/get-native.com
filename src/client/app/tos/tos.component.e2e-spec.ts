1318ec65cd33c6e174dfa4556af208249f874156
Hank Ehly <henry.ehly@gmail.com>
2016-11-10 22:38:38 +0900

    Add TOS files

---
+++ src/client/app/tos/tos.component.e2e-spec.ts
@@ -0,0 +1,6 @@
+/**
+ * tos.component.e2e-spec
+ * get-native.com
+ *
+ * Created by henryehly on 2016/11/10.
+ */
---
+++ src/client/app/tos/tos.component.spec.ts
@@ -0,0 +1,6 @@
+/**
+ * tos.component.spec
+ * get-native.com
+ *
+ * Created by henryehly on 2016/11/10.
+ */
