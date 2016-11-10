11cf236157e559facafcd3108fff84d4dd17022d
Hank Ehly <henry.ehly@gmail.com>
2016-11-10 22:02:43 +0900

    Fix broken tests and modify CSS class names

--- src/client/app/home/home.component.e2e-spec.ts
+++ src/client/app/home/home.component.e2e-spec.ts
@@ -11,10 +11,10 @@ describe('Home', () => {
     });
 
     it('should have large features', () => {
-        expect(element(by.css('.main-features')).isPresent()).toBe(true);
+        expect(element(by.css('.large-features')).isPresent()).toBe(true);
     });
 
     it('should have small features', () => {
-        expect(element(by.css('.secondary-features')).isPresent()).toBe(true);
+        expect(element(by.css('.small-features')).isPresent()).toBe(true);
     });
 });
