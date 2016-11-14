7b29902808b1a6c59e9f5bef3200a1fea0fa8cc0
Hank Ehly <henry.ehly@gmail.com>
2016-11-14 14:15:45 +0900

    Add login modal show/hide functionality

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -5,7 +5,7 @@
  * Created by henryehly on 2016/11/08.
  */
 
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import './operators';
 
 import { Logger } from 'angular2-logger/core';
@@ -13,18 +13,35 @@ import { Logger } from 'angular2-logger/core';
 @Component({
     moduleId: module.id,
     selector: 'gn-app',
-    templateUrl: 'app.component.html'
+    templateUrl: 'app.component.html',
+    encapsulation: ViewEncapsulation.None,
 })
 
 export class AppComponent {
     isCookieCompliant: boolean = false;
+    shouldShowLoginModal: boolean = false;
 
-    constructor(private logger: Logger) {}
+    constructor(private logger: Logger) {
+    }
 
     didComply(): void {
         this.logger.debug('[AppComponent]: didComply()');
         // TODO: Store in cookie / local storage
         // TODO: Cookie / LocalStorage service
         this.isCookieCompliant = true;
+    }
+
+    showLoginModal(): void {
+        this.logger.debug('[AppComponent]: showLoginModal()');
+
+        this.shouldShowLoginModal = true;
+
+        // TODO: You may have to move the 'gn-app' to the <body> tag to avoid doing this
+        document.body.style.overflow = 'hidden';
+    }
+
+    hideLoginModal(): void {
+        this.shouldShowLoginModal = false;
+        document.body.style.overflow = 'visible';
     }
 }
--- src/client/app/core/login/login.component.ts
+++ src/client/app/core/login/login.component.ts
@@ -5,14 +5,27 @@
  * Created by henryehly on 2016/11/13.
  */
 
-import { Component } from '@angular/core';
+import { Component, Output, EventEmitter } from '@angular/core';
 
 @Component({
     moduleId: module.id,
     selector: 'gn-login',
     templateUrl: 'login.component.html',
-    styleUrls: ['login.component.css']
+    styleUrls: ['login.component.css'],
+    animations: [
+        // TODO
+    ]
 })
 
 export class LoginComponent {
+    @Output() didRequestCloseModal = new EventEmitter();
+
+    // TODO: Find 'e' type
+    didClickOverlay(e: any): void {
+        // TODO: This is hacky?
+        if (['click-off-overlay', 'close-button', 'close-button-img'].indexOf(e.target.className) !== -1) {
+            console.log('Close');
+            this.didRequestCloseModal.emit();
+        }
+    }
 }
