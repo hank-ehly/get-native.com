417cc2ae173b37fd49ace96ab5d785a77d54e203
Hank Ehly <henry.ehly@gmail.com>
2016-11-18 08:31:31 +0900

    Add unit tests for login-modal component

--- src/client/app/core/login-modal/login-modal.component.spec_.ts
+++
@@ -1,8 +0,0 @@
-/**
- * login-modal.component.spec
- * get-native.com
- *
- * Created by henryehly on 2016/11/13.
- */
-
-export function main() { }
---
+++ src/client/app/shared/spec-util/spec-util.ts
@@ -0,0 +1,23 @@
+/**
+ * spec-util
+ * get-native.com
+ *
+ * Created by henryehly on 2016/11/18.
+ */
+
+import { DebugElement } from '@angular/core';
+import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+export class SpecUtil {
+    constructor(private fixture: ComponentFixture<any>) {
+    };
+
+    getDebugEl(selector: string): DebugElement {
+        return this.fixture.debugElement.query(By.css(selector));
+    }
+
+    getNativeEl(selector: string): HTMLElement {
+        return this.getDebugEl(selector).nativeElement;
+    }
+}
