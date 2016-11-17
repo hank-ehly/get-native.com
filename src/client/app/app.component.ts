50989394343763c1c2a7e37ca6da873d999cae28
Hank Ehly <henry.ehly@gmail.com>
2016-11-17 10:37:41 +0900

    Fix cookie-compliance animation

--- src/client/app/app.component.ts
+++ src/client/app/app.component.ts
@@ -5,7 +5,7 @@
  * Created by henryehly on 2016/11/08.
  */
 
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit } from '@angular/core';
 import './operators';
 
 import { Logger } from 'angular2-logger/core';
@@ -17,28 +17,25 @@ import { LoginModalComponent, LoginModalService } from './core/index';
     templateUrl: 'app.component.html'
 })
 
-export class AppComponent {
-    isCookieCompliant: boolean = false;
-
+export class AppComponent implements OnInit {
+    showCookieComplianceDialog: boolean;
+    /* Note: Use @ViewChild when you need to access something inside the child; otherwise, connect outputs within template */
     @ViewChild(LoginModalComponent) loginComponent: LoginModalComponent;
 
     constructor(private logger: Logger, private loginModalService: LoginModalService) {
         loginModalService.showModal$.subscribe(() => this.showLoginModal());
     }
 
-    didComply(): void {
-        this.logger.debug('[AppComponent]: didComply()');
-        // TODO: Store in cookie / local storage
-        // TODO: Cookie / LocalStorage service
-        this.isCookieCompliant = true;
+    ngOnInit(): void {
+        /* TODO: Determine via service */
+        this.showCookieComplianceDialog = true;
     }
 
     showLoginModal(): void {
         this.logger.debug('[AppComponent]: showLoginModal()');
-
-        // TODO: You may have to move the 'gn-app' to the <body> tag to avoid doing this
+        /* TODO: Is there a more "Angular" way to do this? */
+        /* REF: http://plnkr.co/edit/AuFMJVHpk9OaLr62puS1?p=preview */
         document.body.style.overflow = 'hidden';
-
         this.loginComponent.isLoginModalVisible = true;
     }
 
