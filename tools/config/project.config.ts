0e2b203be7d90bdce66d728281404e32fb117aaa
Hank Ehly <henry.ehly@gmail.com>
2016-11-21 08:09:18 +0900

    Update to most recent seed

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 
 import { SeedConfig } from './seed.config';
+// import { ExtendPackages } from './seed.config.interfaces';
 
 /**
  * This class extends the basic seed configuration, allowing for project specific overrides. A few examples can be found
@@ -8,35 +9,60 @@ import { SeedConfig } from './seed.config';
  */
 export class ProjectConfig extends SeedConfig {
 
-    PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
+  PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
 
-    constructor() {
-        super();
+  constructor() {
+    super();
+    this.APP_TITLE = 'Get Native';
 
-        // TODO: Change per page
-        this.APP_TITLE = 'Get Native';
+    /* Enable typeless compiler runs (faster) between typed compiler runs. */
+    // this.TYPED_COMPILE_INTERVAL = 5;
 
-        /* Enable typeless compiler runs (faster) between typed compiler runs. */
-        // this.TYPED_COMPILE_INTERVAL = 5;
+    // Add `NPM` third-party libraries to be injected/bundled.
+    this.NPM_DEPENDENCIES = [
+      ...this.NPM_DEPENDENCIES,
+      {src: 'web-animations-js/web-animations.min.js', inject: 'libs'},
+      // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
+      // {src: 'lodash/lodash.min.js', inject: 'libs'},
+    ];
 
-        // Add `NPM` third-party libraries to be injected/bundled.
-        this.NPM_DEPENDENCIES = [
-            ...this.NPM_DEPENDENCIES,
-            {src: 'web-animations-js/web-animations.min.js', inject: 'libs'},
-            // {src: 'jquery/dist/jquery.min.js', inject: 'libs'},
-            // {src: 'lodash/lodash.min.js', inject: 'libs'},
-        ];
+    // Add `local` third-party libraries to be injected/bundled.
+    this.APP_ASSETS = [
+      {src: `${this.CSS_SRC}/vendor/reset.css`, inject: true},
+      ...this.APP_ASSETS,
+      // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
+      // {src: `${this.CSS_SRC}/path-to-lib/test-lib.css`, inject: true, vendor: false},
+    ];
 
-        // Add `local` third-party libraries to be injected/bundled.
-        this.APP_ASSETS = [
-            {src: `${this.CSS_SRC}/vendor/reset.css`, inject: true},
-            ...this.APP_ASSETS,
-            // {src: `${this.APP_SRC}/your-path-to-lib/libs/jquery-ui.js`, inject: true, vendor: false}
-        ];
+    // Add packages (e.g. lodash)
+    // let additionalPackages: ExtendPackages[] = [{
+    //   name: 'lodash',
+    //   path: `${this.APP_BASE}node_modules/lodash/lodash.js`,
+    //   packageMeta: {
+    //     main: 'index.js',
+    //     defaultExtension: 'js'
+    //   }
+    // }];
+    //
+    // or
+    //
+    // let additionalPackages: ExtendPackages[] = [];
+    //
+    // additionalPackages.push({
+    //   name: 'lodash',
+    //   path: `${this.APP_BASE}node_modules/lodash/lodash.js`,
+    //   packageMeta: {
+    //     main: 'index.js',
+    //     defaultExtension: 'js'
+    //   }
+    // });
+    //
+    // this.addPackagesBundles(additionalPackages);
 
-        /* Add to or override NPM module configurations: */
-        // this.mergeObject(this.PLUGIN_CONFIGS['browser-sync'], { ghostMode: false });
+    /* Add to or override NPM module configurations: */
+    // this.mergeObject(this.PLUGIN_CONFIGS['browser-sync'], { ghostMode: false });
 
-        this.ENABLE_SCSS = true;
-    }
+    this.ENABLE_SCSS = true;
+  }
+
 }
