5efe5026c78fc30afbe3dff4bfdee28ca11cdb22
Hank Ehly <henry.ehly@gmail.com>
2016-11-23 16:15:23 +0900

    Add angular logic to registration form

--- src/client/app/core/login/login.module.ts
+++ src/client/app/core/login/login.module.ts
@@ -7,11 +7,12 @@
 
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 import { LoginService, LoginComponent, SocialLoginComponent, RegisterComponent, EmailLoginComponent } from './index';
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule],
     declarations: [LoginComponent, SocialLoginComponent, EmailLoginComponent, RegisterComponent],
     exports: [LoginComponent, SocialLoginComponent, EmailLoginComponent, RegisterComponent],
     providers: [LoginService]
--- src/client/app/core/login/register/register.component.ts
+++ src/client/app/core/login/register/register.component.ts
@@ -5,7 +5,10 @@
  * Created by henryehly on 2016/11/23.
  */
 
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, ViewChild, AfterViewChecked } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Logger } from 'angular2-logger/core';
 
 @Component({
     moduleId: module.id,
@@ -14,10 +17,115 @@ import { Component, EventEmitter, Output } from '@angular/core';
     styleUrls: ['register.component.css']
 })
 
-export class RegisterComponent {
+export class RegisterComponent implements AfterViewChecked {
     @Output() setModalView = new EventEmitter<string>();
+    @ViewChild('form') currentForm: NgForm;
+    formRef: NgForm;
+    passwordStrength: string;
+    passwordStrengthNumber: number;
+
+    meetsWeakRequirements: boolean = false;
+    meetsGoodRequirements: boolean = false;
+    meetsExcellentRequirements: boolean = false;
+
+    credentials = {
+        email: '',
+        password: '',
+        passwordConfirm: ''
+    };
+
+    formErrors = {
+        'email': '',
+        'password': '',
+        'password-confirm': ''
+    };
+
+    // Example
+    // validationMessages = {
+    //     'name': {
+    //         'required':      'Name is required.',
+    //         'minlength':     'Name must be at least 4 characters long.',
+    //         'maxlength':     'Name cannot be more than 24 characters long.',
+    //         'forbiddenName': 'Someone named "Bob" cannot be a hero.'
+    //     },
+    //     'power': {
+    //         'required': 'Power is required.'
+    //     }
+    // };
+
+    constructor(private logger: Logger) {
+        this.passwordStrength = 'TOO SHORT';
+    }
+
+    ngAfterViewChecked(): void {
+        this.formChanged();
+    }
+
+    formChanged() {
+        if (this.currentForm === this.formRef) return;
+
+        this.formRef = this.currentForm;
+        if (this.formRef) {
+            this.formRef.valueChanges.subscribe(data => this.onValueChanged(data));
+        }
+    }
 
     onSetModalView(view: string) {
         this.setModalView.emit(view);
+    }
+
+    onSubmit(): void {
+        this.logger.debug('Submit!');
+    }
+
+    onValueChanged(data?: any) {
+        this.logger.debug('Value Change', data);
+
+        if (data['password']) {
+            this.calculatePasswordStrength(data['password']);
+        }
+
+        if (!this.formRef) return;
+        const form = this.formRef.form;
+
+        for (const field in this.formErrors) {
+            /* Reset error messages if present */
+            this.formErrors[field] = '';
+            const control = form.get(field);
+
+            // Example
+            // if (control && control.dirty && !control.valid) {
+            //     const messages = this.validationMessages[field];
+            //     for (const key in control.errors) {
+            //         this.formErrors[field] += messages[key] + ' ';
+            //     }
+            // }
+        }
+    }
+
+    calculatePasswordStrength(password: string): void {
+        if (password.length === 0) return;
+
+        if (password.length >= 8) {
+            this.meetsWeakRequirements = true;
+            this.meetsGoodRequirements = true;
+            this.meetsExcellentRequirements = true;
+            this.passwordStrength = 'EXCELLENT';
+        } else if (password.length >= 5) {
+            this.meetsWeakRequirements = true;
+            this.meetsGoodRequirements = true;
+            this.meetsExcellentRequirements = false;
+            this.passwordStrength = 'GOOD';
+        } else if (password.length >= 2) {
+            this.meetsWeakRequirements = true;
+            this.meetsGoodRequirements = false;
+            this.meetsExcellentRequirements = false;
+            this.passwordStrength = 'WEAK';
+        } else {
+            this.meetsWeakRequirements = false;
+            this.meetsGoodRequirements = false;
+            this.meetsExcellentRequirements = false;
+            this.passwordStrength = 'TOO SHORT';
+        }
     }
 }
