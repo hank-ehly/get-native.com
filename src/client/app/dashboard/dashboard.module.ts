246a72b751863e7bb7c314d61850769856b74fec
Hank Ehly <henry.ehly@gmail.com>
2016-11-28 07:23:37 +0900

    Add DashboardComponent boilerplate

---
+++ src/client/app/dashboard/dashboard.module.ts
@@ -0,0 +1,18 @@
+/**
+ * dashboard.module
+ * get-native.com
+ *
+ * Created by henryehly on 2016/11/28.
+ */
+
+import { NgModule } from '@angular/core';
+
+import { DashboardComponent } from './index';
+
+@NgModule({
+    declarations: [DashboardComponent],
+    exports: [DashboardComponent]
+})
+
+export class DashboardModule {
+}
