207ef63e2dd1aad9d010d80854755115311837cc
Hank Ehly <henry.ehly@gmail.com>
2016-12-04 14:10:22 +0900

    Fix failing tests

--- src/e2e/specs/help.component.e2e-spec.ts
+++ src/e2e/specs/help.component.e2e-spec.ts
@@ -11,19 +11,19 @@ describe('Help', () => {
     });
 
     it('should have visible faq headers', () => {
-        let nFaqHeaders = $$('.faq-list__faq-header').count();
+        let nFaqHeaders = $$('.faqs__faq-header').count();
         expect(nFaqHeaders).toBeGreaterThan(0);
     });
 
     it('all faqs should be collapsed', () => {
-        let nExpandedFaqs = $$('.faq-list__faq-detail').count();
+        let nExpandedFaqs = $$('.faqs__faq-detail').count();
         expect(nExpandedFaqs).toBe(0);
     });
 
     it('should display a faq detail when a faq header is selected', () => {
-        $$('.faq-list__faq-title').first().click();
+        $$('.faqs__faq-title').first().click();
 
-        let nExpandedFaqs = $$('.faq-list__faq-detail').count();
+        let nExpandedFaqs = $$('.faqs__faq-detail').count();
         expect(nExpandedFaqs).toBe(1);
     });
 });
