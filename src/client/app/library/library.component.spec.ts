e87adb07f3b176bf42c504ddafa7721bfdc2c7ad
Hank Ehly <henry.ehly@gmail.com>
2016-12-05 07:10:12 +0900

    Add LibraryComponent stub

---
+++ src/client/app/library/library.component.spec.ts
@@ -0,0 +1,8 @@
+/**
+ * library.component.spec
+ * get-native.com
+ *
+ * Created by henryehly on 2016/12/05.
+ */
+
+
---
+++ src/e2e/specs/library.component.e2e-spec.ts
@@ -0,0 +1,12 @@
+/**
+ * library.component.e2e-spec
+ * get-native.com
+ *
+ * Created by henryehly on 2016/12/05.
+ */
+
+describe('LibraryComponent', () => {
+    it('STUB', () => {
+        expect(true).toBeTruthy();
+    });
+});
