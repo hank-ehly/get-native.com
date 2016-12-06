ed2bcab0ae0c27b53dfb6a4f86455c4f53de701e
Hank Ehly <henry.ehly@gmail.com>
2016-12-06 21:45:02 +0900

    Finish category dropdown styling

--- src/client/app/library/library.component.ts
+++ src/client/app/library/library.component.ts
@@ -21,6 +21,7 @@ import { Logger } from 'angular2-logger/core';
 export class LibraryComponent implements OnInit {
     isDropdownVisible: boolean;
     videos: any[];
+    categories: any[];
 
     constructor(private logger: Logger, private navbarService: NavbarService) {
     }
@@ -28,23 +29,29 @@ export class LibraryComponent implements OnInit {
     ngOnInit(): void {
         this.logger.debug('[LibraryComponent]: ngOnInit()');
         this.navbarService.setTitle('Library');
+
+        /* Mock */
         this.videos = [
-            {isPlaceholder: false},
-            {isPlaceholder: false},
-            {isPlaceholder: false},
-
-            {isPlaceholder: false},
-            {isPlaceholder: false},
-            {isPlaceholder: false},
-
-            {isPlaceholder: false},
-            {isPlaceholder: false},
-            {isPlaceholder: false},
-
-            {isPlaceholder: false},
-            {isPlaceholder: false},
-            {isPlaceholder: false}
+            {isPlaceholder: false}, {isPlaceholder: false}, {isPlaceholder: false},
+            {isPlaceholder: false}, {isPlaceholder: false}, {isPlaceholder: false},
+            {isPlaceholder: false}, {isPlaceholder: false}, {isPlaceholder: false},
+            {isPlaceholder: false}, {isPlaceholder: false}, {isPlaceholder: false}
         ];
+
+        /* Mock */
+        let categoriesArr = [
+            {title: 'Business', subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3']},
+            {title: 'Holidays', subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3']},
+            {title: 'Negotiations', subcategories: ['Subcategory 1', 'Subcategory 2']},
+            {title: 'Language', subcategories: ['Subcategory 1', 'Subcategory 2']},
+            {title: 'Hobbies', subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3', 'Subcategory 4', 'Subcategory 5']},
+            {title: 'Travel', subcategories: ['Subcategory 1', 'Subcategory 2']},
+        ];
+
+        /* Move out of component */
+        this.categories = categoriesArr.map((e, i) => {
+            return i % 3 === 0 ? categoriesArr.slice(i, i + 3) : null;
+        }).filter((e) => e);
     }
 
     onToggleDropdown(): void {
