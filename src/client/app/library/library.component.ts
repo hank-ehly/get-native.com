dfeaff75a1aedbb344d9fcba2f2f00d0929516ad
Hank Ehly <henry.ehly@gmail.com>
2016-12-06 22:17:59 +0900

    Allow number of categories not divisible by 3

--- src/client/app/library/library.component.ts
+++ src/client/app/library/library.component.ts
@@ -32,26 +32,43 @@ export class LibraryComponent implements OnInit {
 
         /* Mock */
         this.videos = [
-            {isPlaceholder: false}, {isPlaceholder: false}, {isPlaceholder: false},
-            {isPlaceholder: false}, {isPlaceholder: false}, {isPlaceholder: false},
-            {isPlaceholder: false}, {isPlaceholder: false}, {isPlaceholder: false},
-            {isPlaceholder: false}, {isPlaceholder: false}, {isPlaceholder: false}
+            {placeholder: false}, {placeholder: false}, {placeholder: false},
+            {placeholder: false}, {placeholder: false}, {placeholder: false},
+            {placeholder: false}, {placeholder: false}, {placeholder: false},
+            {placeholder: false}, {placeholder: false}, {placeholder: false}
         ];
 
         /* Mock */
-        let categoriesArr = [
-            {title: 'Business', subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3']},
-            {title: 'Holidays', subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3']},
+        let categories = [
+            {title: 'Business',     subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3', 'Subcategory 4']},
+            {title: 'Holidays',     subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3']},
             {title: 'Negotiations', subcategories: ['Subcategory 1', 'Subcategory 2']},
-            {title: 'Language', subcategories: ['Subcategory 1', 'Subcategory 2']},
-            {title: 'Hobbies', subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3', 'Subcategory 4', 'Subcategory 5']},
-            {title: 'Travel', subcategories: ['Subcategory 1', 'Subcategory 2']},
+            {title: 'Language',     subcategories: ['Subcategory 1', 'Subcategory 2']},
+            {title: 'Hobbies',      subcategories: ['Subcategory 1', 'Subcategory 2', 'Subcategory 3', 'Subcategory 4', 'Subcategory 5']},
+            {title: 'Travel',       subcategories: ['Subcategory 1', 'Subcategory 2']},
+            {title: 'Sports',       subcategories: ['Subcategory 1']}
         ];
 
-        /* Move out of component */
-        this.categories = categoriesArr.map((e, i) => {
-            return i % 3 === 0 ? categoriesArr.slice(i, i + 3) : null;
+        /* TODO: Move out of component */
+        let chunkSize = 3;
+        this.categories = categories.map((e, i) => {
+            return i % chunkSize === 0 ? categories.slice(i, i + 3) : null;
         }).filter((e) => e);
+
+        let surplus = categories.length % 3;
+        if (surplus !== 0) {
+            let spaceLeft = chunkSize - surplus;
+
+            let i = 0;
+            while (i < spaceLeft) {
+                let lastIndex = this.categories.length - 1;
+                let placeholder = {placeholder: true};
+                this.categories[lastIndex].push(placeholder);
+                i++;
+            }
+        }
+
+        this.logger.debug('[LibraryComponent]: Categories: ', this.categories);
     }
 
     onToggleDropdown(): void {
