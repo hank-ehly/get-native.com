45c2c50bf07ecf4d9df2649b812149b3dd5604a3
Hank Ehly <henry.ehly@gmail.com>
2016-12-06 14:34:12 +0900

    Add notification indicator with in/out animation

--- src/client/app/shared/navbar/navbar.component.ts
+++ src/client/app/shared/navbar/navbar.component.ts
@@ -5,7 +5,7 @@
  * Created by henryehly on 2016/11/06.
  */
 
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, trigger, transition, animate, keyframes, style } from '@angular/core';
 
 import { LoginService, NavbarService } from '../../core/index';
 
@@ -15,12 +15,31 @@ import { Logger } from 'angular2-logger/core';
     moduleId: module.id,
     selector: 'gn-navbar',
     templateUrl: 'navbar.component.html',
-    styleUrls: ['navbar.component.css']
+    styleUrls: ['navbar.component.css'],
+    animations: [
+        trigger('bounceInOut', [
+            transition(':enter', [
+                animate(100, keyframes([
+                    style({transform: 'scale(0.9)', offset: 0}),
+                    style({transform: 'scale(1.3)', offset: 0.7}),
+                    style({transform: 'scale(1)', offset: 1.0})
+                ]))
+            ]),
+            transition(':leave', [
+                animate(100, keyframes([
+                    style({transform: 'scale(1)', offset: 0}),
+                    style({transform: 'scale(1.3)', offset: 0.7}),
+                    style({transform: 'scale(0.9)', offset: 1.0})
+                ]))
+            ])
+        ])
+    ]
 })
 export class NavbarComponent implements OnInit {
     @Input() authenticated: boolean;
     title: string;
     logoLinkPath: string;
+    isNotificationIndicatorVisible: boolean = true;
 
     constructor(private loginService: LoginService,
                 private logger: Logger,
@@ -36,5 +55,10 @@ export class NavbarComponent implements OnInit {
         this.logger.debug('[NavbarComponent]: requestShowLoginModal()');
         event.preventDefault();
         this.loginService.showModal();
+    }
+
+    /* MOCK */
+    toggleNotificationIndicator() {
+        this.isNotificationIndicatorVisible = !this.isNotificationIndicatorVisible;
     }
 }
