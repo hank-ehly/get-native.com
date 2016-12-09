559e3648787102babb144f1eedc6c0a4ef265683
Hank Ehly <henry.ehly@gmail.com>
2016-12-09 18:42:30 +0900

    Add settings module stub

---
+++ src/client/app/settings/notifications/notifications.component.ts
@@ -0,0 +1,22 @@
+/**
+ * notifications.component
+ * get-native.com
+ *
+ * Created by henryehly on 2016/12/09.
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'gn-notifications',
+    templateUrl: 'notifications.component.html',
+    styleUrls: ['notifications.component.css']
+})
+export class NotificationsComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
