9e2d54c44bdbd96e8f1b49ca6cb9f385388dd943
Hank Ehly <henry.ehly@gmail.com>
2016-12-11 16:00:00 +0900

    Import SharedModule instead of CommonModule

--- src/client/app/login/login.module.ts
+++ src/client/app/login/login.module.ts
@@ -6,7 +6,6 @@
  */
 
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import {
@@ -16,10 +15,11 @@ import {
     EmailLoginComponent,
     PasswordStrengthComponent
 } from './index';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     imports: [
-        CommonModule,
+        SharedModule,
         FormsModule
     ],
     declarations: [
