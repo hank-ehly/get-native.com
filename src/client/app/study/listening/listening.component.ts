192f3ca7b8833e5921510bfd18d40e11a6405afc
Hank Ehly <henry.ehly@gmail.com>
2016-12-11 14:47:07 +0900

    Display listening component as default

--- src/client/app/study/listening/listening.component.ts
+++ src/client/app/study/listening/listening.component.ts
@@ -7,6 +7,10 @@
 
 import { Component, OnInit } from '@angular/core';
 
+import { NavbarService } from '../../core/navbar/navbar.service';
+
+import { Logger } from 'angular2-logger/core';
+
 @Component({
     moduleId: module.id,
     selector: 'gn-listening',
@@ -14,9 +18,11 @@ import { Component, OnInit } from '@angular/core';
     styleUrls: ['listening.component.css']
 })
 export class ListeningComponent implements OnInit {
-    constructor() {
+    constructor(private logger: Logger, private navbar: NavbarService) {
     }
 
     ngOnInit() {
+        this.logger.info('[ListeningComponent] ngOnInit()');
+        this.navbar.setTitle('LISTENING');
     }
 }
