043021ec0aa3cd51c859d61efd646f7be9f84540
Hank Ehly <henry.ehly@gmail.com>
2016-12-11 14:32:19 +0900

    Add listening component stub

---
+++ src/client/app/study/listening/listening.component.ts
@@ -0,0 +1,22 @@
+/**
+ * listening.component
+ * get-native.com
+ *
+ * Created by henryehly on 2016/12/11.
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'gn-listening',
+    templateUrl: 'listening.component.html',
+    styleUrls: ['listening.component.css']
+})
+export class ListeningComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
