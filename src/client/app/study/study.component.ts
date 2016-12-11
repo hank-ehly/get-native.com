bb6fd6a2c3007d261536d8ae6bbc846955c05f8f
Hank Ehly <henry.ehly@gmail.com>
2016-12-11 14:47:07 +0900

    Display listening component as default

--- src/client/app/study/study.component.ts
+++ src/client/app/study/study.component.ts
@@ -6,6 +6,7 @@
  */
 
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 import { Logger } from 'angular2-logger/core';
 
@@ -16,9 +17,20 @@ import { Logger } from 'angular2-logger/core';
     styleUrls: ['study.component.css']
 })
 export class StudyComponent implements OnInit {
-    constructor(private logger: Logger) {
+    activePage: string;
+
+    constructor(private logger: Logger, private route: ActivatedRoute) {
     }
 
     ngOnInit() {
+        this.logger.info('[StudyComponent] ngOnInit()');
+        this.route.params.subscribe(this.paramsChanged.bind(this));
+    }
+
+    paramsChanged(params: Params): void {
+        this.logger.info('[StudyComponent] paramsChanged: ', params);
+        if (params['page']) {
+            this.activePage = params['page'];
+        }
     }
 }
--- src/client/app/study/study.module.ts
+++ src/client/app/study/study.module.ts
@@ -10,10 +10,11 @@ import { NgModule } from '@angular/core';
 import { StudyComponent } from './study.component';
 import { SharedModule } from '../shared/shared.module';
 import { StudyRoutingModule } from './study-routing.module';
+import { ListeningComponent } from './listening/listening.component';
 
 @NgModule({
     imports: [SharedModule, StudyRoutingModule],
-    declarations: [StudyComponent]
+    declarations: [StudyComponent, ListeningComponent]
 })
 export class StudyModule {
 }
