d305f9bd47155827fb3f7518800c6b4e30737f80
Hank Ehly <henry.ehly@gmail.com>
2016-12-12 13:17:07 +0900

    Add transition component

---
+++ src/client/app/study/transition/transition.component.ts
@@ -0,0 +1,22 @@
+/**
+ * transition.component
+ * get-native.com
+ *
+ * Created by henryehly on 2016/12/12.
+ */
+
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    moduleId: module.id,
+    selector: 'gn-transition',
+    templateUrl: 'transition.component.html',
+    styleUrls: ['transition.component.css']
+})
+export class TransitionComponent implements OnInit {
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+}
