19a5729b8982f2c6bd4bd665f2fb0c5cf83de5f4
Hank Ehly <henry.ehly@gmail.com>
2016-12-14 22:20:49 +0900

    Add toolbar animation

--- src/client/app/shared/toolbar/toolbar.component.ts
+++ src/client/app/shared/toolbar/toolbar.component.ts
@@ -5,7 +5,7 @@
  * Created by henryehly on 2016/11/06.
  */
 
-import { Component } from '@angular/core';
+import { Component, trigger, transition, style, animate, keyframes } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Logger } from 'angular2-logger/core';
@@ -14,16 +14,58 @@ import { Logger } from 'angular2-logger/core';
     moduleId: module.id,
     selector: 'gn-toolbar',
     templateUrl: 'toolbar.component.html',
-    styleUrls: ['toolbar.component.css']
+    styleUrls: ['toolbar.component.css'],
+    animations: [
+        trigger('enterUpLeaveDown', [
+            transition(':enter', [
+                animate('300ms ease-in-out', keyframes([
+                    style({opacity: 0, transform: 'translateY(15px)', offset: 0}),
+                    style({opacity: 0.8, transform: 'translateY(-3px)', offset: 0.3}),
+                    style({opacity: 1, transform: 'translateY(0)', offset: 1})
+                ]))
+            ]),
+            transition(':leave', [
+                animate('200ms ease-in-out', keyframes([
+                    style({opacity: 1, transform: 'translateY(0)', offset: 0}),
+                    style({opacity: 0, transform: 'translateY(15px)', offset: 1})
+                ]))
+            ])
+        ])
+    ]
 })
 
 export class ToolbarComponent {
+    isTooltipVisible: boolean;
+
+    /* Todo: Where do you get these languages from? */
+    languages: string[] = ['日本語', 'ITALIANO', 'ESPAÑOL', 'ENGLISH'];
+
+    /* Todo: Which language should be the default selected language? */
+    selectedLang: string;
+
     constructor(private logger: Logger, private router: Router) {
+        this.selectedLang = this.languages[0];
     }
 
     /* TODO: Implement */
     onLogout(): void {
         this.logger.info('[ToolbarComponent]: onLogout()');
         this.router.navigate(['']);
+    }
+
+    onShowTooltip(): void {
+        this.isTooltipVisible = true;
+    }
+
+    onHideTooltip(): void {
+        this.isTooltipVisible = false;
+    }
+
+    onToggleTooltip(): void {
+        this.isTooltipVisible = !this.isTooltipVisible;
+    }
+
+    setSelectedLanguage(lang: string) {
+        this.selectedLang = lang;
     }
 }
---
+++ src/client/app/shared/toolbar/tooltip.component.spec.ts
@@ -0,0 +1,54 @@
+/**
+ * tooltip.component.spec
+ * get-native.com
+ *
+ * Created by henryehly on 2016/12/14.
+ */
+
+import { DebugElement } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { APP_BASE_HREF } from '@angular/common';
+import { ComponentFixture, TestBed, async } from '@angular/core/testing';
+
+import { ToolbarComponent } from './toolbar.component';
+import { SpecUtil, STUBLogger } from '../../core/index';
+
+import { Logger } from 'angular2-logger/core';
+
+export function main() {
+    let comp: ToolbarComponent;
+    let fixture: ComponentFixture<ToolbarComponent>;
+    let de: DebugElement;
+    let util: SpecUtil;
+
+    describe('ToolbarComponent', () => {
+        beforeEach(async(() => {
+            TestBed.configureTestingModule({
+                imports: [RouterModule.forRoot([])],
+                declarations: [ToolbarComponent],
+                providers: [
+                    {provide: Logger, useValue: STUBLogger},
+                    {provide: APP_BASE_HREF, useValue: '<%= APP_BASE %>'}
+                ]
+            }).compileComponents().then(() => {
+                fixture = TestBed.createComponent(ToolbarComponent);
+                util = new SpecUtil(fixture);
+                comp = fixture.componentInstance;
+                fixture.detectChanges();
+            });
+        }));
+
+        it('should hide the tooltip by default', () => {
+            de = util.getDebugEl('.toolbar__tooltip-languages');
+            expect(de).toBeNull();
+        });
+
+        it ('should initialize the tooltip after the visibility property becomes \'true\'', () => {
+            comp.isTooltipVisible = true;
+            fixture.detectChanges();
+
+            de = util.getDebugEl('.toolbar__tooltip-languages');
+            expect(de).not.toBeNull();
+        });
+    });
+}
