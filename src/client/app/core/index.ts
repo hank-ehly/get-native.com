7d113e1339e8f62f2f3e9f5911a5a4f847a6dd84
Hank Ehly <henry.ehly@gmail.com>
2016-12-20 22:03:05 +0900

    Use new UnitInterval type to describe video progress & time

--- src/client/app/core/index.ts
+++ src/client/app/core/index.ts
@@ -12,3 +12,4 @@ export * from './spec/index';
 export * from './password/index';
 export * from './login/login.service';
 export * from './time-format/time-format.service';
+export * from './typings/unit-interval';
