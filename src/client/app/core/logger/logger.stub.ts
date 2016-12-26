649a43a65a393cfea4c1ac68d6cdbcb95aa79f70
Hank Ehly <henry.ehly@gmail.com>
2016-12-26 14:55:37 +0900

    Fix broken tests

--- src/client/app/core/logger/logger.stub.ts
+++ src/client/app/core/logger/logger.stub.ts
@@ -10,5 +10,9 @@ import { Logger } from './logger';
 export const STUBLogger: Logger = <Logger>{
     debug(message?: any, ...optionalParams: any[]): void {
         return;
+    },
+
+    info(message?: any, ...optionalParams: any[]): void {
+        return;
     }
 };
