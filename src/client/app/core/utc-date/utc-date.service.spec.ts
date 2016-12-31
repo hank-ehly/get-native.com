9e05ae54ee4369c13a2df2ac7751a095c4080be6
Hank Ehly <henry.ehly@gmail.com>
2016-12-31 09:23:31 -0500

    Add utc-date service

---
+++ src/client/app/core/utc-date/utc-date.service.spec.ts
@@ -0,0 +1,58 @@
+/**
+ * date.service.spec
+ * get-native.com
+ *
+ * Created by henryehly on 2016/12/31.
+ */
+
+import { UTCDateService } from './utc-date.service';
+
+export function main() {
+    let service: UTCDateService;
+    let datetime: string;
+    let date: Date;
+
+    describe('UTCDateService', () => {
+        beforeAll(() => {
+            service = new UTCDateService();
+            datetime = 'Thu Dec 14 04:35:55 +0000 2017';
+            date = service.parse(datetime);
+        });
+
+        it('should parse UTC datetime string into a JavaScript Date object.', () => {
+            expect(typeof date).toEqual('object');
+        });
+
+        it('should parse the correct year', () => {
+            expect(date.getUTCFullYear()).toEqual(2017);
+        });
+
+        it('should parse the correct month', () => {
+            expect(date.getUTCMonth()).toEqual(11);
+        });
+
+        it('should parse the correct date', () => {
+            expect(date.getUTCDate()).toEqual(14);
+        });
+
+        it('should parse the correct hour', () => {
+            expect(date.getUTCHours()).toEqual(4);
+        });
+
+        it('should parse the correct minute', () => {
+            expect(date.getUTCMinutes()).toEqual(35);
+        });
+
+        it('should parse the correct second', () => {
+            expect(date.getUTCSeconds()).toEqual(55);
+        });
+
+        it('should provide the string representation of the parsed day of the week', () => {
+            expect(service.getWeekDay(date)).toEqual('Thu');
+        });
+
+        it('should provide the text representation of the parsed month', () => {
+            expect(service.getTextMonth(date)).toEqual('Dec');
+        });
+    });
+}
---
+++ src/client/app/core/utc-date/utc-date.service.ts
@@ -0,0 +1,38 @@
+/**
+ * utc-date.service
+ * get-native.com
+ *
+ * Created by henryehly on 2016/12/31.
+ */
+
+import { Injectable } from '@angular/core';
+
+import { DateTime } from '../typings/datetime';
+import { WEEKDAYS } from './weekdays';
+import { MONTHS } from './months';
+
+@Injectable()
+export class UTCDateService {
+    parse(datetime: DateTime): Date {
+        let units: string[] = datetime.split(' ');
+
+        let year    = +units[units.length - 1];
+        let month   = MONTHS.indexOf(units[1]);
+        let date    = +units[2];
+
+        let time    = units[3].split(':');
+        let hours   = +time[0];
+        let minutes = +time[1];
+        let seconds = +time[2];
+
+        return new Date(Date.UTC(year, month, date, hours, minutes, seconds));
+    }
+
+    getWeekDay(date: Date): string {
+        return WEEKDAYS[date.getUTCDay()];
+    }
+
+    getTextMonth(date: Date): string {
+        return MONTHS[date.getUTCMonth()];
+    }
+}
