4909ab7e3af14fd90e363761f249662a51836f4b
Hank Ehly <henry.ehly@gmail.com>
2017-01-11 14:24:37 +0900

    Change FuzzyNumberPipe selector

--- src/client/app/shared/fuzzy-number/fuzzy-number.pipe.ts
+++ src/client/app/shared/fuzzy-number/fuzzy-number.pipe.ts
@@ -8,7 +8,7 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-    name: 'fuzzyNumber'
+    name: 'fuzzy'
 })
 export class FuzzyNumberPipe implements PipeTransform {
     transform(value: any, args: any[]): any {
