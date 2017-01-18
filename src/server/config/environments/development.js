023dac9cc3a0d3c2ee5fe84dd9e7d5f92b8f67ae
Hank Ehly <henry.ehly@gmail.com>
2017-01-18 22:26:59 +0900

    Remove commented out old code

--- src/server/config/environments/development.js
+++ src/server/config/environments/development.js
@@ -4,13 +4,3 @@
  *
  * Created by henryehly on 2017/01/18.
  */
-
-// Example
-//
-// export default {
-//     env: 'development',
-//     MONGOOSE_DEBUG: true,
-//     jwtSecret: '0a6b944d-d2fb-46fc-a85e-0295c986cd9f',
-//     db: 'mongodb://localhost/express-mongoose-es6-rest-api-development',
-//     port: 4040
-// };
--- src/server/index.js
+++ src/server/index.js
@@ -5,67 +5,13 @@
  * Created by henryehly on 2017/01/15.
  */
 
-// Redo
-    // handle command line args first (if applicable)
-    // load OS ENV vars (if applicable) nconf.env();
-    // load vars from config/environments (if applicable)
-    // initialize database
-    // start server
+// Todo
+// handle command line args first (if applicable)
+// load OS ENV vars (if applicable) nconf.env();
+// load vars from config/environments (if applicable)
+// initialize database
+// start server
 
 const server = require('./config/initializers/server');
 
 server();
-
-////////// old
-
-// const app        = require('express')();
-// const bodyParser = require('body-parser');
-//
-// const routes     = require('./config/routes');
-//
-// /* Todo: Create folder for middleware */
-//
-// /* Todo: What exactly does this do? */
-// app.use(bodyParser.json());
-//
-// /* Todo: Move to separate middleware file */
-// app.use((req, res, next) => {
-//     res.set('Access-Control-Allow-Origin', '*');
-//
-//     /* Todo: Clarify difference between Expose & Allow */
-//     res.set('Access-Control-Expose-Headers', 'X-GN-Auth-Token');
-//     res.set('Access-Control-Allow-Headers', 'Authorization, Content-Type');
-//
-//     next();
-// });
-//
-// /* Todo: Move to middleware */
-// /* Todo: Cleanup */
-// /* Todo: Implement */
-// const fs  = require('fs');
-// const jwt = require('jsonwebtoken');
-// app.use((req, res, next) => {
-//
-//     if (req.path === '/login') {
-//         next();
-//         return;
-//     }
-//
-//     let publicKey = fs.readFileSync(`${__dirname}/keys/id_rsa.pem`);
-//     let token = req.get('Authorization').split(' ')[1];
-//     jwt.verify(token, publicKey, {algorithms: ['RS256']}, (err, decoded) => {
-//         if (err) {
-//             throw new Error(err);
-//         }
-//
-//         console.log(decoded);
-//
-//         next();
-//     });
-// });
-//
-// app.use(routes);
-//
-// app.listen(3000, () => {
-//     console.log('Listening on port 3000\n');
-// });
