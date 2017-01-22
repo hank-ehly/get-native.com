1c3df49839e45381ba58f73a640edbad36512c12
Hank Ehly <henry.ehly@gmail.com>
2017-01-22 14:31:10 +0900

    Consolidate routes into bundle file

--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -5,24 +5,19 @@
  * Created by henryehly on 2017/01/18.
  */
 
-const express = require('express');
-
-// Todo: Consolidate
-const auth = require('../app/controllers/auth');
-const categories = require('../app/controllers/categories');
-const cuedVideos = require('../app/controllers/cued-videos');
-const study = require('../app/controllers/study');
-const videos = require('../app/controllers/videos');
+const express     = require('express');
+const controllers = require('../app/controllers');
 
 const router = express.Router();
 
 // Todo: express-validations
-router.post('/login', auth.login);
-router.get('/categories', categories.list);
-router.get('/cued_videos', cuedVideos.list);
-router.get('/study/stats', study.stats);
-router.get('/videos', videos.list);
-router.get('/videos/:id', videos.show);
-router.post('/videos/:id/like', videos.like);
+
+router.post('/login', controllers.auth.login);
+router.get('/categories', controllers.categories.list);
+router.get('/cued_videos', controllers.cuedVideos.list);
+router.get('/study/stats', controllers.study.stats);
+router.get('/videos', controllers.videos.list);
+router.get('/videos/:id', controllers.videos.show);
+router.post('/videos/:id/like', controllers.videos.like);
 
 module.exports = router;
