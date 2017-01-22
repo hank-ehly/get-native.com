f41e7dda2bc3ad18f8b4abfa0241ba33ce9a93c5
Hank Ehly <henry.ehly@gmail.com>
2017-01-22 14:31:10 +0900

    Consolidate routes into bundle file

--- src/server/app/controllers/index.js
+++ src/server/app/controllers/index.js
@@ -5,4 +5,16 @@
  * Created by henryehly on 2017/01/18.
  */
 
+const auth       = require('./auth');
+const categories = require('./categories');
+const cuedVideos = require('./cued-videos');
+const study      = require('./study');
+const videos     = require('./videos');
 
+module.exports = {
+    auth: auth,
+    categories: categories,
+    cuedVideos: cuedVideos,
+    study: study,
+    videos: videos
+};
