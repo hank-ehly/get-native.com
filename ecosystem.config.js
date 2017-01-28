163f1e2526425a6619c12319094650fecf2e5cdc
Hank Ehly <henry.ehly@gmail.com>
2017-01-28 20:21:44 +0900

    Use pm2 cluster mode

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,3 +1,6 @@
+const currentDir = '/var/www/get-native.com/current';
+const logDir     = '/var/log';
+
 module.exports = {
     apps: [
         {
@@ -6,22 +9,22 @@ module.exports = {
             error_file: './api.dev.err.log',
             out_file: './api.dev.out.log',
             pid_file: './api.dev.pid',
-            exec_mode: 'fork',
+            exec_mode: 'cluster',
             merge_logs: true,
-            watch: ['src/server', 'ecosystem.config.js'],
+            watch: ['./src/server', './ecosystem.config.js'],
             interpreter: '/Users/henryehly/.nvm/versions/node/v6.9.4/bin/node',
             env: {
                 NODE_ENV: 'development'
             }
         },
         {
-            name: 'api.stg.get-native.com',
-            script: '/var/www/get-native.com/current/src/server/index.js',
-            cwd: '/var/www/get-native.com/current',
-            error_file: '/var/log/pm2/api.stg.get-native.com.err.log',
-            out_file: '/var/log/pm2/api.stg.get-native.com.out.log',
-            pid_file: '/run/pm2/api.stg.get-native.com.pid',
-            exec_mode: 'fork',
+            name: 'api.stg',
+            script: `${currentDir}/src/server/index.js`,
+            cwd: currentDir,
+            error_file: `${logDir}/pm2/api.stg.err.log`,
+            out_file: `${logDir}/pm2/api.stg.out.log`,
+            pid_file: '/run/pm2/api.stg.pid',
+            exec_mode: 'cluster',
             combine_logs: true,
             interpreter: '/usr/local/nodejs-binary/bin/node',
             env: {
