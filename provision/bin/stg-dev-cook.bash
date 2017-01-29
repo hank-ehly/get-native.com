1369d7afa7ebf0f0205bf57d58fd89f527c26da8
Hank Ehly <henry.ehly@gmail.com>
2017-01-29 09:42:30 +0900

    Replace incorrect 'development' environment with correct 'staging' environment

--- provision/bin/stg-dev-cook.bash
+++ provision/bin/stg-dev-cook.bash
@@ -6,8 +6,8 @@ read -p "You are about to provision the staging server. Continue? [y/N] "
 CHOICE=`echo $REPLY | tr [:upper:] [:lower:]`
 
 if [[ $CHOICE = 'y' ]]; then
-        echo 'bundle exec knife solo cook root@stg.get-native.com -E development -i ~/.ssh/stg.get-native.com/provision.web'
-        bundle exec knife solo cook root@stg.get-native.com -E development -i ~/.ssh/stg.get-native.com/provision.web
+        echo 'bundle exec knife solo cook root@stg.get-native.com -E staging -i ~/.ssh/stg.get-native.com/provision.web'
+        bundle exec knife solo cook root@stg.get-native.com -E staging -i ~/.ssh/stg.get-native.com/provision.web
 else
         echo 'Aborted.' && exit 0
 fi
