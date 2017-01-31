b7754268516f62af272145d09110b81fd8f1ed02
Hank Ehly <henry.ehly@gmail.com>
2017-01-31 21:36:05 +0900

    Fix syntax error causing password strength meter not to change color

--- src/client/app/shared/password-strength/password-strength.component.ts
+++ src/client/app/shared/password-strength/password-strength.component.ts
@@ -17,25 +17,26 @@ import { PasswordService } from '../../core/index';
 })
 export class PasswordStrengthComponent {
     score: number = 0;
+
+    matrix: [number, string][] = [
+        [0, 'VERY WEAK'],
+        [20, 'WEAK'],
+        [40, 'GOOD'],
+        [60, 'STRONG'],
+        [80, 'VERY STRONG']
+    ];
+
     private _strengthLabel: string;
 
     constructor(private passwordService: PasswordService) {
     }
 
     get strengthLabel(): string {
-        let matrix: [number, string][] = [
-            [0, 'VERY WEAK'],
-            [20, 'WEAK'],
-            [40, 'GOOD'],
-            [60, 'STRONG'],
-            [80, 'VERY STRONG']
-        ];
+        this._strengthLabel = this.matrix[0][1];
 
-        this._strengthLabel = matrix[0][1];
-
-        for (const row in matrix) {
-            let min = matrix[row][0];
-            let txt = matrix[row][1];
+        for (const row in this.matrix) {
+            let min = this.matrix[row][0];
+            let txt = this.matrix[row][1];
             if (this.score >= min) this._strengthLabel = txt;
         }
 
