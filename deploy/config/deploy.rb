9fcade33737a60770aca16e916fcab29237c4588
Hank Ehly <henry.ehly@gmail.com>
2017-02-01 22:41:54 +0900

    Add secrets folder to capistrano linked_dirs

--- deploy/config/deploy.rb
+++ deploy/config/deploy.rb
@@ -5,6 +5,7 @@ set :repo_url, 'git@github.com:hank-ehly/get-native.com.git'
 set :deploy_to, "/var/www/#{fetch(:application)}"
 set :scm, :git
 set :keep_releases, 5
+set :linked_dirs, %w(src/server/config/secrets)
 
 after 'deploy:updated', :setup do
     on roles(:web) do
