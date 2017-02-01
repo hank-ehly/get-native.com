e88c54a2b06c334ed4f426f90cbd71e91ba95051
Hank Ehly <henry.ehly@gmail.com>
2017-02-02 08:28:05 +0900

    Declare email regex in component to prevent system js compilation error

--- tools/env/base.ts
+++ tools/env/base.ts
@@ -1,10 +1,7 @@
 import { EnvConfig } from './env-config.interface';
 
 const BaseConfig: EnvConfig = {
-    moderator: 'getnative.moderator@gmail.com',
-
-    /* From HTML5 Specification */
-    EMAIL_REGEX: '[a-z0-9!#$%&\'*+/=?^_`{|}~.-]+@[a-z0-9-]+(\.[a-z0-9-]+)*'
+    moderator: 'getnative.moderator@gmail.com'
 };
 
 export = BaseConfig;
