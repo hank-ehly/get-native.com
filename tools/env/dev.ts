97894aa5fb27f2e0ec3f95852c4d83786574d2cb
Hank Ehly <henry.ehly@gmail.com>
2017-02-20 13:38:32 +0700

    Load facebook javascript sdk

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -2,7 +2,8 @@ import { EnvConfig } from './env-config.interface';
 
 const DevConfig: EnvConfig = {
     API: 'http://localhost:3000',
-    ENV: 'DEV'
+    ENV: 'DEV',
+    facebookAppId: '215586025582003'
 };
 
 export = DevConfig;
