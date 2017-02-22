542ab780926410ab4caaa336bdbd347250aa79e2
Hank Ehly <henry.ehly@gmail.com>
2017-02-23 07:34:25 +0900

    Add starting schema implementation

--- src/server/app/controllers/categories.js
+++ src/server/app/controllers/categories.js
@@ -5,7 +5,10 @@
  * Created by henryehly on 2017/01/18.
  */
 
+const Category = require('../models/Category');
+
 module.exports.list = (req, res) => {
-    let mockList = require('../../mock/categories.json');
-    res.send(mockList);
+    // let mockList = require('../../mock/categories.json');
+    // res.send(mockList);
+    Category.findAll().then(categories => res.send(categories));
 };
--- src/server/config/environments/base.js
+++ src/server/config/environments/base.js
@@ -5,21 +5,32 @@
  * Created by henryehly on 2017/01/30.
  */
 
-const nconf      = require('nconf');
-const fs         = require('fs');
+const nconf = require('nconf');
+const fs = require('fs');
 
 const secretsDir = __dirname + '/../secrets';
 
 let publicKey = fs.readFile(secretsDir + '/id_rsa.pem', (err, data) => {
-    if (err) throw new Error(err);
+    if (err) {
+        throw new Error(err);
+    }
 
     nconf.set('publicKey', data.toString());
 });
 
 let privateKey = fs.readFile(secretsDir + '/id_rsa', (err, data) => {
-    if (err) throw new Error(err);
+    if (err) {
+        throw new Error(err);
+    }
 
     nconf.set('privateKey', data.toString());
 });
 
 nconf.set('PORT', 3000);
+
+let dbToken = 'get_native_' + nconf.get('NODE_ENV').toLowerCase();
+nconf.set('db:name', dbToken);
+nconf.set('db:user', dbToken);
+nconf.set('db:pass', dbToken);
+nconf.set('db:port', 3306);
+nconf.set('db:host', 'localhost');
