09922899ed5808e2544b557c66087e9118bddb3e
Hank Ehly <henry.ehly@gmail.com>
2017-02-24 10:54:56 +0900

    Add account_id column to notifications table

---
+++ src/server/db/migrations/20170224014248-add-account-id-to-notifications.js
@@ -0,0 +1,24 @@
+/**
+ * 20170224014248-add-account-id-to-notifications
+ * get-native.com
+ *
+ * Created by henryehly on 2017/01/24.
+ */
+
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.addColumn('notifications', 'account_id', {
+            type: Sequelize.INTEGER,
+            references: {
+                model: 'accounts',
+                key: 'id'
+            },
+            onUpdate: 'restrict',
+            onDelete: 'restrict'
+        });
+    },
+
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.removeColumn('notifications', 'account_id');
+    }
+};
