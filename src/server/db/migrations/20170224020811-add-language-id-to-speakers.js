208ec8bf2370a217a935dc3af9e2d443de322172
Hank Ehly <henry.ehly@gmail.com>
2017-02-24 11:24:49 +0900

    Add associations to speakers schema
    - Due to naming collision, language_id is temporarily unavailable

---
+++ src/server/db/migrations/20170224020811-add-language-id-to-speakers.js
@@ -0,0 +1,25 @@
+/**
+ * 20170224020811-add-language-id-to-speakers
+ * get-native.com
+ *
+ * Created by henryehly on 2017/01/24.
+ */
+
+// FIX: https://github.com/sequelize/sequelize/issues/5212
+// module.exports = {
+//     up: function(queryInterface, Sequelize) {
+//         return queryInterface.addColumn('speakers', 'language_id', {
+//             type: Sequelize.INTEGER,
+//             references: {
+//                 model: 'languages',
+//                 key: 'id'
+//             },
+//             onUpdate: 'restrict',
+//             onDelete: 'restrict'
+//         });
+//     },
+//
+//     down: function(queryInterface, Sequelize) {
+//         return queryInterface.removeColumn('speakers', 'language_id');
+//     }
+// };
