edb6fe392e78cb4353a54ebe1135e62b7c50e178
Hank Ehly <henry.ehly@gmail.com>
2017-02-24 10:54:41 +0900

    Add cued_videos model/migrations

---
+++ src/server/app/models/cued-video.js
@@ -0,0 +1,13 @@
+/**
+ * cued-video
+ * get-native.com
+ *
+ * Created by henryehly on 2017/02/24.
+ */
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('CuedVideo', {}, {
+        tableName: 'cued_videos',
+        underscored: true
+    });
+};
