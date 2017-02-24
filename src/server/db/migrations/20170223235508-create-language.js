fffa9c204accd6cf1d26b28601d7ef64b80079fd
Hank Ehly <henry.ehly@gmail.com>
2017-02-24 09:02:37 +0900

    Add 'Language' model/migration

---
+++ src/server/db/migrations/20170223235508-create-language.js
@@ -0,0 +1,40 @@
+/**
+ * 20170223235508-create-language
+ * get-native.com
+ *
+ * Created by henryehly on 2017/01/24.
+ */
+
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('languages', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            name: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue: ''
+            },
+            code: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue: ''
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('languages');
+    }
+};
