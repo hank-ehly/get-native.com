1b8f6ea1d92a01f44e6871386b4579605cf90b91
Hank Ehly <henry.ehly@gmail.com>
2017-02-24 10:54:41 +0900

    Add cued_videos model/migrations

---
+++ src/server/db/migrations/20170224014712-create-cued-video.js
@@ -0,0 +1,43 @@
+/**
+ * 20170224014712-create-cued-video
+ * get-native.com
+ *
+ * Created by henryehly on 2017/01/24.
+ */
+
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('cued_videos', {
+            video_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'videos',
+                    key: 'id'
+                },
+                onUpdate: 'restrict',
+                onDelete: 'restrict'
+            },
+            account_id: {
+                type: Sequelize.INTEGER,
+                references: {
+                    model: 'accounts',
+                    key: 'id'
+                },
+                onUpdate: 'restrict',
+                onDelete: 'restrict'
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('cued_videos');
+    }
+};
