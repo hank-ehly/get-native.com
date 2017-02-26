673da3b9da0cfed580cf2dfbfe7e45a3ff7f54f5
Hank Ehly <henry.ehly@gmail.com>
2017-02-26 09:54:50 +0900

    Add writing_questions table

---
+++ src/server/db/migrations/20170226005153-create-writing-question.js
@@ -0,0 +1,39 @@
+/**
+ * 20170226005153-create-writing-question
+ * get-native.com
+ *
+ * Created by henryehly on 2017/02/26.
+ */
+
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return queryInterface.createTable('writing_questions', {
+            id: {
+                allowNull: false,
+                autoIncrement: true,
+                primaryKey: true,
+                type: Sequelize.INTEGER
+            },
+            text: {
+                type: Sequelize.STRING,
+                allowNull: false,
+                defaultValue: ''
+            },
+            example_answer: {
+                type: Sequelize.TEXT,
+                allowNull: false
+            },
+            created_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            },
+            updated_at: {
+                allowNull: false,
+                type: Sequelize.DATE
+            }
+        });
+    },
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.dropTable('writing_questions');
+    }
+};
