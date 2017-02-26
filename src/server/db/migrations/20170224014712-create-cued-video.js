c4d255b3a2bd25afff1eb1e0e4900d40b8c55eb8
Hank Ehly <henry.ehly@gmail.com>
2017-02-26 14:30:32 +0900

    Make CURRENT_TIMESTAMP the default value for all created_at / updated_at columns

--- src/server/db/migrations/20170224014712-create-cued-video.js
+++ src/server/db/migrations/20170224014712-create-cued-video.js
@@ -28,11 +28,13 @@ module.exports = {
             },
             created_at: {
                 allowNull: false,
-                type: Sequelize.DATE
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('NOW')
             },
             updated_at: {
                 allowNull: false,
-                type: Sequelize.DATE
+                type: Sequelize.DATE,
+                defaultValue: Sequelize.fn('NOW')
             }
         });
     },
