99b34a7c935cd49158cd3f87d071dd40f47023f7
Hank Ehly <henry.ehly@gmail.com>
2017-02-26 09:54:50 +0900

    Add writing_questions table

---
+++ src/server/app/models/writing-question.js
@@ -0,0 +1,23 @@
+/**
+ * writing-question
+ * get-native.com
+ *
+ * Created by henryehly on 2017/02/26.
+ */
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('WritingQuestion', {
+        text: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            defaultValue: ''
+        },
+        example_answer: {
+            type: DataTypes.TEXT,
+            allowNull: false
+        }
+    }, {
+        tableName: 'writing_questions',
+        underscored: true
+    });
+};
