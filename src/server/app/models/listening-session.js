5610465869a6b386a81af8e3034601aa2530bb07
Hank Ehly <henry.ehly@gmail.com>
2017-02-26 11:18:28 +0900

    Create listening_sessions model/migration

---
+++ src/server/app/models/listening-session.js
@@ -0,0 +1,13 @@
+/**
+ * listening-session
+ * get-native.com
+ *
+ * Created by henryehly on 2017/02/26.
+ */
+
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('ListeningSession', {}, {
+        tableName: 'listening_sessions',
+        underscored: true
+    });
+};
