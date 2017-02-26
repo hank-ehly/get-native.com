539a6322429228766aa405afa536040d93a4b33a
Hank Ehly <henry.ehly@gmail.com>
2017-02-26 09:46:39 +0900

    Add video_id to transcripts table migration

---
+++ src/server/db/migrations/20170226004422-add-video-id-to-transcripts.js
@@ -0,0 +1,25 @@
+/**
+ * 20170226004422-add-video-id-to-transcripts
+ * get-native.com
+ *
+ * Created by henryehly on 2017/02/26.
+ */
+
+// FIX: https://github.com/sequelize/sequelize/issues/5212
+// module.exports = {
+//     up: function(queryInterface, Sequelize) {
+//         return queryInterface.addColumn('transcripts', 'video_id', {
+//             type: Sequelize.INTEGER,
+//             references: {
+//                 model: 'videos',
+//                 key: 'id'
+//             },
+//             onUpdate: 'restrict',
+//             onDelete: 'restrict'
+//         });
+//     },
+//
+//     down: function(queryInterface, Sequelize) {
+//         return queryInterface.removeColumn('transcripts', 'video_id');
+//     }
+// };
