8db621340a107f4245c84fbfba1fc5705cc808ef
Hank Ehly <henry.ehly@gmail.com>
2017-02-28 08:30:16 +0900

    Use chance library for cued_videos seed data

--- src/server/db/seeders/20170227220658-cued_videos.js
+++ src/server/db/seeders/20170227220658-cued_videos.js
@@ -10,16 +10,18 @@ const models = require('../../app/models');
 const Video = models.Video;
 const Account = models.Account;
 
-const Helper = require('../seed-helper');
+const chance = require('chance').Chance();
 
 module.exports = {
     up: function(queryInterface, Sequelize) {
         return Promise.all([Video.min('id'), Video.max('id'), Account.min('id'), Account.max('id')]).then(x => {
             const cuedVideos = [];
-            const numCuedVideos = 5000;
 
-            for (let i = 0; i < numCuedVideos; i++) {
-                cuedVideos.push({video_id: Helper.rand(x[0], x[1]), account_id: Helper.rand(x[2], x[3])});
+            for (let i = 0; i < 5000; i++) {
+                cuedVideos.push({
+                    video_id: chance.integer({min: x[0], max: x[1]}),
+                    account_id: chance.integer({min: x[2], max: x[3]})
+                });
             }
 
             return queryInterface.bulkInsert('cued_videos', cuedVideos);
