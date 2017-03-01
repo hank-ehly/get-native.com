638f0e9e127feedd8fc987d87bbbbf130409c80f
Hank Ehly <henry.ehly@gmail.com>
2017-03-01 18:33:23 +0900

    Add collocations seed data

---
+++ src/server/db/seeders/20170301091520-collocations.js
@@ -0,0 +1,38 @@
+/**
+ * 20170301091520-collocations
+ * get-native.com
+ *
+ * Created by henryehly on 2017/03/01.
+ */
+
+const Transcript = require('../../app/models').Transcript;
+const chance = require('chance').Chance();
+
+module.exports = {
+    up: function(queryInterface, Sequelize) {
+        return Promise.all([Transcript.min('id'), Transcript.max('id')]).then(x => {
+            const collocations = [];
+
+            // for each transcript
+            for (let i = x[0]; i < x[1]; i++) {
+
+                // create between 5 ~ 10 collocations
+                let numCollocations = chance.integer({min: 5, max: 10});
+                for (let j = 0; j < numCollocations; j++) {
+
+                    collocations.push({
+                        description: chance.sentence(),
+                        text: chance.sentence({words: chance.integer({min: 1, max: 4})}),
+                        transcript_id: i
+                    });
+                }
+            }
+
+            return queryInterface.bulkInsert('collocations', collocations);
+        });
+    },
+
+    down: function(queryInterface, Sequelize) {
+        return queryInterface.bulkDelete('collocations');
+    }
+};
