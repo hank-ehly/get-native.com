b382ff1a72806d885d51d6185966e3559a57db64
Hank Ehly <henry.ehly@gmail.com>
2017-03-04 09:50:16 +0900

    Add mock categories response logic

--- src/server/app/controllers/categories.js
+++ src/server/app/controllers/categories.js
@@ -5,10 +5,42 @@
  * Created by henryehly on 2017/01/18.
  */
 
-const Category = require('../models/Category');
+const models = require('../models');
+
+const Category = models.Category;
+const Subcategory = models.Subcategory;
 
 module.exports.list = (req, res) => {
-    // let mockList = require('../../mock/categories.json');
-    // res.send(mockList);
-    Category.findAll().then(categories => res.send(categories));
+    Category.findAll().then(categories => {
+        Subcategory.findAll().then(subcategories => {
+
+            let response = {
+                records: categories,
+                count: categories.length
+            };
+
+            for (let i = 0; i < response.records.length; i++) {
+                let record = response.records[i];
+
+                record.topics = {
+                    records: [],
+                    count: 0
+                };
+
+                for (let j = 0; j < subcategories.length; j++) {
+                    if (subcategories[j].category_id === record.id) {
+                        record.topics.records.push({
+                            id: subcategories[j].id,
+                            id_str: subcategories[j].id,
+                            name: subcategories[j].name
+                        });
+
+                        record.topics.count += 1;
+                    }
+                }
+            }
+
+            res.send(response);
+        });
+    });
 };
