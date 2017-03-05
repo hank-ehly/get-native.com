42069967d40f228373224df0d7d45d8b23cfcb00
Hank Ehly <henry.ehly@gmail.com>
2017-03-06 07:59:36 +0900

    Don't use AuthGuard in home-routing.module because of e2e timeout

--- src/client/app/home/home-routing.module.ts
+++ src/client/app/home/home-routing.module.ts
@@ -9,12 +9,10 @@ import { Routes, RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 
 import { HomeComponent } from './home.component';
-import { AuthGuard } from '../core/auth/auth-guard.service';
 
 const homeRoutes: Routes = [
     {
         path: '',
-        canActivate: [AuthGuard],
         component: HomeComponent,
         data: {
             title: 'Home'
