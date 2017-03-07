295901c4260da3e2323d6ae629293e564f341e04
Hank Ehly <henry.ehly@gmail.com>
2017-03-07 14:13:17 +0900

    Don't log sequelize initialization in test/circle_ci environments

--- src/server/config/initializers/database.js
+++ src/server/config/initializers/database.js
@@ -8,6 +8,7 @@
 const Sequelize = require('sequelize');
 const nconf = require('nconf');
 const dbconf = require('../../db/database.js');
+const logger = require('../logger');
 
 module.exports = () => {
     let e = nconf.get('env');
@@ -15,7 +16,8 @@ module.exports = () => {
     const sequelize = new Sequelize(dbconf[e].database, dbconf[e].username, dbconf[e].password, {
         host: dbconf[e].host,
         dialect: dbconf[e].dialect,
-        port: 3306
+        port: 3306,
+        logging: ['test', 'circle_ci'].includes(e) ? false : logger.info
     });
 
     return sequelize.authenticate();
