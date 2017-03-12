40e43d41d22cc13930676b3ec8810733a185a039
Hank Ehly <henry.ehly@gmail.com>
2017-03-12 10:00:17 +0900

    Add basic implementation for GET /study/writing_answers endpoint

--- src/server/app/controllers/study.js
+++ src/server/app/controllers/study.js
@@ -5,12 +5,35 @@
  * Created by henryehly on 2017/01/18.
  */
 
+const models = require('../models');
+const WritingAnswer = models.WritingAnswer;
+const WritingQuestion = models.WritingQuestion;
+
 module.exports.stats = (req, res) => {
     let mock = require('../../mock/study_stats.json');
     res.send(mock);
 };
 
 module.exports.writing_answers = (req, res) => {
-    let mock = require('../../mock/study_writing_answers.json');
-    res.send(mock);
+    WritingAnswer.findAll({
+        where: {},
+        attributes: ['id', 'answer', 'created_at'],
+        include: [
+            {
+                model: WritingQuestion,
+                as: 'writing_question',
+                attributes: ['text']
+            }
+        ],
+        limit: 10
+    }).then(answers => {
+        let response = {
+            count: answers.length,
+            records: answers
+        };
+
+        res.send(response);
+    }).catch(error => {
+        res.send(error);
+    });
 };
