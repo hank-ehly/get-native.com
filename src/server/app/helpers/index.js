c2620d792ea5b6d0ce507ca2f25591e54f7b2f02
Hank Ehly <henry.ehly@gmail.com>
2017-03-16 08:29:17 +0900

    Normalize helper module names

--- src/server/app/helpers/index.js
+++ src/server/app/helpers/index.js
@@ -13,8 +13,11 @@ let helpers = {};
 fs.readdirSync(__dirname).filter((file) => {
     return (file.indexOf('.') !== 0) && (file !== path.basename(module.filename)) && (file.slice(-3) === '.js');
 }).forEach((file) => {
-    let fileNameNoExt = file.substring(0, file.length - 3);
-    helpers[fileNameNoExt] = require(path.join(__dirname, file));
+    let moduleName = file.substring(0, file.length - 3)
+        .replace(/\b\w/g, s => s.toUpperCase())
+        .replace(/-/g, '');
+
+    helpers[moduleName] = require(path.join(__dirname, file));
 });
 
 module.exports = helpers;
