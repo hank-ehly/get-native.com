e622faae134ecea053703e6d70b285d522226e1f
Hank Ehly <henry.ehly@gmail.com>
2017-03-15 14:47:35 +0900

    Add utility method for extracting X-GN-Auth-Token header from request object

--- src/server/spec/helpers/utility.spec.js
+++ src/server/spec/helpers/utility.spec.js
@@ -9,27 +9,64 @@ const util = require('../../app/helpers')['utility'];
 const assert = require('assert');
 
 describe('utility', () => {
-    it('should return \'string\' if passed a string', () => {
-        assert.equal(util.typeof('hello world'), 'string');
+
+    describe('typeof', () => {
+        it(`should return 'string' if passed a string`, () => {
+            assert.equal(util.typeof('hello world'), 'string');
+        });
+
+        it(`should return 'boolean' if passed a boolean`, () => {
+            assert.equal(util.typeof(false), 'boolean');
+        });
+
+        it(`should return 'array' if passed a array`, () => {
+            assert.equal(util.typeof([1, 2, 3]), 'array');
+        });
+
+        it(`should return 'object' if passed a object`, () => {
+            assert.equal(util.typeof({foo: 'bar'}), 'object');
+        });
+
+        it(`should return 'number' if passed a number`, () => {
+            assert.equal(util.typeof(90), 'number');
+        });
+
+        it(`should return 'function' if passed a function`, () => {
+            assert.equal(util.typeof(function() {
+            }), 'function');
+        });
     });
 
-    it('should return \'boolean\' if passed a boolean', () => {
-        assert.equal(util.typeof(false), 'boolean');
-    });
+    describe('extractAuthTokenFromRequest', () => {
+        it('should throw a RangeError if the number of arguments is less than 1', () => {
+            assert.throws(() => util.extractAuthTokenFromRequest(), RangeError);
+        });
 
-    it('should return \'array\' if passed a array', () => {
-        assert.equal(util.typeof([1, 2, 3]), 'array');
-    });
+        it('should throw a RangeError if the number of arguments is greater than 1', () => {
+            assert.throws(() => util.extractAuthTokenFromRequest({}, {}), RangeError);
+        });
 
-    it('should return \'object\' if passed a object', () => {
-        assert.equal(util.typeof({foo: 'bar'}), 'object');
-    });
+        it(`should throw a TypeError if the first argument does not have a 'headers' property of object type`, () => {
+            assert.throws(() => util.extractAuthTokenFromRequest({foo: 'bar'}), TypeError);
+        });
 
-    it('should return \'number\' if passed a number', () => {
-        assert.equal(util.typeof(90), 'number');
-    });
+        it(`should throw a ReferenceError if the request object does not have an 'authorization' property of string type`, () => {
+            assert.throws(() => util.extractAuthTokenFromRequest({headers: {foo: 'bar'}}), ReferenceError);
+        });
 
-    it('should return \'function\' if passed a function', () => {
-        assert.equal(util.typeof(function() {}), 'function');
+        it(`should throw a SyntaxError if the requests' 'authorization' header field value is not correctly formatted`, () => {
+            assert.throws(() => util.extractAuthTokenFromRequest({headers: {authorization: ''}}), SyntaxError);
+        });
+
+        it('should return a string', () => {
+            let retVal = util.extractAuthTokenFromRequest({headers: {authorization: 'Bearer XXX.XXX.XXX'}});
+            let typeOfRetVal = util.typeof(retVal);
+            assert.equal('string', typeOfRetVal);
+        });
+
+        it('should return a string whose length is greater than 0', () => {
+            let retVal = util.extractAuthTokenFromRequest({headers: {authorization: 'Bearer XXX.XXX.XXX'}});
+            assert(retVal.length > 0);
+        });
     });
 });
