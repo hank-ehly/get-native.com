dd247748a96ff4a763a7c9ea631cc98feb30a65b
Hank Ehly <henry.ehly@gmail.com>
2017-03-18 10:45:43 +0900

    Add validation methods to spec-util

--- src/server/spec/spec-util.js
+++ src/server/spec/spec-util.js
@@ -6,37 +6,43 @@
  */
 
 const request = require('supertest');
-const exec = require('child_process').exec;
+const exec    = require('child_process').exec;
 
-module.exports = {
-    defaultTimeout: 30000,
+module.exports.defaultTimeout = 30000;
 
-    seedAll: function(done) {
-        this.seedAllUndo(function() {
-            exec('npm run sequelize db:seed:all', function() {
-                done();
-            });
-        });
-    },
-
-    seedAllUndo: function(done) {
-        exec('npm run sequelize db:seed:undo:all', function() {
+module.exports.seedAll = function(done) {
+    this.seedAllUndo(function() {
+        exec('npm run sequelize db:seed:all', function() {
             done();
         });
-    },
+    });
+};
 
-    login: function(cb) {
-        delete require.cache[require.resolve('../index')];
+module.exports.seedAllUndo = function(done) {
+    exec('npm run sequelize db:seed:undo:all', function() {
+        done();
+    });
+};
 
-        require('../index').then(function(_) {
-            server = _;
+module.exports.login = function(cb) {
+    delete require.cache[require.resolve('../index')];
 
-            request(_).post('/login').send({
-                email: 'test@email.com',
-                password: 'test_password'
-            }).then(function(response) {
-                cb(_, 'Bearer: ' + response.header['x-gn-auth-token'], response.body);
-            });
+    require('../index').then(function(_) {
+        server = _;
+
+        request(_).post('/login').send({
+            email: 'test@email.com',
+            password: 'test_password'
+        }).then(function(response) {
+            cb(_, 'Bearer: ' + response.header['x-gn-auth-token'], response.body);
         });
-    }
+    });
+};
+
+module.exports.isNumber = function(value) {
+    return new RegExp(/^[0-9]+$/).test(value);
+};
+
+module.exports.isParsableDateValue = function(value) {
+    return new Date(value).toDateString() !== 'Invalid Date';
 };
