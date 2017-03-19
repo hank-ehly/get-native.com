9c0b09b8193b14b127d169a7c9a367c44e0a9354
Hank Ehly <henry.ehly@gmail.com>
2017-03-19 13:46:26 +0900

    Specify singular forms of relations in video model

--- src/server/app/models/video.js
+++ src/server/app/models/video.js
@@ -32,8 +32,8 @@ module.exports = function(sequelize, DataTypes) {
         tableName: 'videos',
         underscored: true,
         associations: function(models) {
-            models.Video.belongsTo(models.Speaker);
-            models.Video.belongsTo(models.Subcategory);
+            models.Video.belongsTo(models.Speaker, {as: 'speaker'});
+            models.Video.belongsTo(models.Subcategory, {as: 'subcategory'});
             models.Video.hasMany(models.CuedVideo, {as: 'cued_videos'});
             models.Video.hasMany(models.Like, {as: 'likes'});
             models.Video.hasMany(models.StudySession, {as: 'study_sessions'});
