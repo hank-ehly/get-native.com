9382763e33dfc9a054b3f56c798de6b1d63e1ee1
Hank Ehly <henry.ehly@gmail.com>
2017-03-20 13:13:11 +0900

    Implement the rest of GET /videos

--- src/server/app/controllers/videos.js
+++ src/server/app/controllers/videos.js
@@ -10,20 +10,23 @@ const Video           = models.Video;
 const Speaker         = models.Speaker;
 const Category        = models.Category;
 const Subcategory     = models.Subcategory;
+const Language        = models.Language;
 const ResponseWrapper = require('../helpers').ResponseWrapper;
 
-module.exports.index = (req, res) => {
+module.exports.index = (req, res, next) => {
     const conditions = {};
-    const limit      = req.query.count || 9;
-
-    conditions.language_code = req.query.lang || 'en';
+    const limit      = +req.query.count || 9;
 
     if (req.query.max_id) {
         conditions.id = {$gte: +req.query.max_id};
     }
 
-    // todo: Query can be improved
-    findAllSubcategoryIdsFromReqQuery(req).then(subcategoryIds => {
+    return Promise.all([findLanguageCodeFromReqQuery(req.query), findAllSubcategoryIdsFromReqQuery(req.query)]).then(results => {
+        const languageCode   = results[0];
+        const subcategoryIds = results[1];
+
+        conditions.language_code = languageCode;
+
         if (subcategoryIds.length) {
             conditions.subcategory_id = {$in: subcategoryIds};
         }
@@ -32,30 +35,52 @@ module.exports.index = (req, res) => {
             attributes: {exclude: ['speaker_id', 'subcategory_id', 'language_code', 'updated_at']},
             where: conditions,
             include: [
-                {
-                    model: Speaker,
-                    attributes: ['name'],
-                    as: 'speaker'
-                }, {
-                    model: Subcategory,
-                    attributes: ['name'],
-                    as: 'subcategory'
-                }
+                {model: Speaker, attributes: ['name'], as: 'speaker'},
+                {model: Subcategory, attributes: ['name'], as: 'subcategory'}
             ],
             limit: limit
         }).then(videos => {
             let videosAsJson = ResponseWrapper.wrap(videos);
             res.send(videosAsJson);
         });
+    }).catch(e => {
+        return next(e);
     });
 };
 
-function findAllSubcategoryIdsFromReqQuery(req) {
+function findLanguageCodeFromReqQuery(query) {
     return new Promise((resolve, reject) => {
-        if (req.query.subcategory_id) {
-            resolve([req.query.subcategory_id]);
-        } else if (req.query.category_id) {
-            resolve(findAllSubcategoryIdsForCategoryId(req.query.category_id));
+        if (!query.lang) {
+            return resolve('en');
+        }
+
+        return Language.findOne({
+            where: {code: query.lang},
+            attributes: ['code']
+        }).then(l => {
+            if (l) {
+                resolve(l.code);
+            } else {
+                reject({
+                    message: 'Validation Failed',
+                    errors: [
+                        {
+                            message: `'${query.lang}' is not a valid language code`,
+                            path: 'lang'
+                        }
+                    ]
+                });
+            }
+        });
+    });
+}
+
+function findAllSubcategoryIdsFromReqQuery(query) {
+    return new Promise((resolve, reject) => {
+        if (query.subcategory_id) {
+            resolve([query.subcategory_id]);
+        } else if (query.category_id) {
+            resolve(findAllSubcategoryIdsForCategoryId(query.category_id));
         } else {
             resolve([]);
         }
