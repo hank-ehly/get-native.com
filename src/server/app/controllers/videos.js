581032ba7fd80d9e9c636112290c56a47e33e30f
Hank Ehly <henry.ehly@gmail.com>
2017-03-21 08:48:00 +0900

    Add todo comment

--- src/server/app/controllers/videos.js
+++ src/server/app/controllers/videos.js
@@ -59,6 +59,12 @@ module.exports.index = (req, res, next) => {
 };
 
 module.exports.show = (req, res, next) => {
+    // query(sql, [options={}]) -> Promise
+    // [options.nest=false]
+    // If true, transforms objects with . separated property names into nested objects using dottie.js.
+    // For example { 'user.username': 'john' } becomes { user: { username: 'john' }}.
+    // When nest is true, the query type is assumed to be 'SELECT', unless otherwise specified
+
     const accountId = AuthHelper.extractAccountIdFromRequest(req);
 
     const likeCountAllPromise = Like.count({where: ['video_id = ?', +req.params.id]});
