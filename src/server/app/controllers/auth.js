44e1f14097af01e1c02a9fb81211dad0554e6e64
Hank Ehly <henry.ehly@gmail.com>
2017-03-26 19:37:04 +0900

    Add more tests and implementation logic to POST /registration

--- src/server/app/controllers/auth.js
+++ src/server/app/controllers/auth.js
@@ -5,42 +5,101 @@
  * Created by henryehly on 2017/01/18.
  */
 
-const nconf   = require('nconf');
-const jwt     = require('jsonwebtoken');
-const logger  = require('../../config/logger');
-const Account = require('../models').Account;
+const nconf      = require('nconf');
+const jwt        = require('jsonwebtoken');
+const logger     = require('../../config/logger');
+const Account    = require('../models').Account;
 const AuthHelper = require('../helpers').Auth;
+const nodemailer = require('nodemailer');
+const Promise    = require('bluebird');
+const mailer     = require('../../config/initializers/mailer');
+const fs         = Promise.promisifyAll(require('fs'));
+const k          = require('../../config/keys.json');
 
 module.exports.login = (req, res, next) => {
     const attributes = [
-        'id', 'email', 'browser_notifications_enabled', 'email_notifications_enabled', 'email_verified', 'default_study_language_code',
-        'picture_url', 'is_silhouette_picture'
+        k.Attr.Id,
+        k.Attr.Email,
+        k.Attr.BrowserNotificationsEnabled,
+        k.Attr.EmailNotificationsEnabled,
+        k.Attr.EmailVerified,
+        k.Attr.DefaultStudyLanguageCode,
+        k.Attr.PictureUrl,
+        k.Attr.IsSilhouettePicture
     ];
 
-    Account.find({where: {email: req.body.email}, attributes: attributes}).then(account => {
-        AuthHelper.generateTokenForAccountId(account.id, (err, token) => {
-            if (err) {
-                throw new Error(err);
-            }
+    let account = null;
+    Account.find({where: {email: req.body[k.Attr.Email]}, attributes: attributes}).then(_account => {
+        if (!_account) {
+            throw new Error(`No account found for email '${req.body[k.Attr.Email]}'`);
+        }
 
-            AuthHelper.setAuthHeadersOnResponseWithToken(res, token);
-            const accountAsJson = account.get({plain: true});
-            res.send(accountAsJson);
-        });
-    }).catch(() => {
-        next({
+        account = _account;
+        return AuthHelper.generateTokenForAccountId(account.id);
+    }).then(token => {
+        AuthHelper.setAuthHeadersOnResponseWithToken(res, token);
+        const accountAsJson = account.get({plain: true});
+        res.send(accountAsJson);
+    }).catch((e) => {
+        return next({
             message: 'Authentication Error',
             errors: [{message: 'Account does not exist'}]
-        })
+        });
+    });
+};
+
+module.exports.register = (req, res, next) => {
+    const email    = req.body[k.Attr.Email];
+    const password = req.body[k.Attr.Password];
+    let account    = null;
+
+    Account.existsForEmail(req.body[k.Attr.Email]).then(exists => {
+        if (exists) {
+            return next({
+                message: 'Error',
+                errors: [{message: 'Account already exists'}]
+            });
+        }
+
+        return Account.create({email: email, password: password})
+    }).then(_account => {
+        account = _account;
+        const htmlTemplate = __dirname + '/../templates/confirm-registration.html';
+        const textTemplate = __dirname + '/../templates/confirm-registration.txt';
+        return Promise.all([fs.readFileAsync(htmlTemplate), fs.readFileAsync(textTemplate)]);
+    }).spread((htmlTemplate, textTemplate) => {
+        const message = {
+            from: nconf.get(k.NoReply),
+            to: email,
+            subject: 'Welcome to Get Native!', // todo: 'translate' helper like Rails
+            text: textTemplate.toString(),
+            html: htmlTemplate.toString()
+        };
+
+        return mailer.sendMail(message).then(() => {
+            return AuthHelper.generateTokenForAccountId(account.id);
+        });
+
+    }).then(token => {
+        AuthHelper.setAuthHeadersOnResponseWithToken(res, token);
+        const accountAsJson = account.get({plain: true});
+        res.send(accountAsJson);
+    }).catch((e) => {
+        console.log(e);
+
+        next({
+            message: 'Error',
+            errors: [{message: 'Failed to query existing accounts'}]
+        });
     });
 };
 
 module.exports.authenticate = (req, res, next) => {
     AuthHelper.validateRequest(req, (err, token) => {
-        if (err) throw new Error(err);
+        if (err) return next(err);
 
         AuthHelper.refreshToken(token, (err, token) => {
-            if (err) throw new Error(err);
+            if (err) return next(err);
 
             AuthHelper.setAuthHeadersOnResponseWithToken(res, token);
             next();
