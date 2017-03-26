95c9eaeddc2bc6f9985cf04dfed73f4460d6ad26
Hank Ehly <henry.ehly@gmail.com>
2017-03-26 12:10:57 +0900

    Add error handler to GET /speakers endpoint

--- src/server/app/controllers/speakers.js
+++ src/server/app/controllers/speakers.js
@@ -9,6 +9,12 @@ const Speaker = require('../models').Speaker;
 
 module.exports.show = (req, res, next) => {
     Speaker.findById(req.params.id, {attributes: {exclude: ['created_at', 'updated_at']}}).then(speaker => {
-        res.send(speaker.toJSON());
+        let speakerAsJson = speaker.toJSON();
+        res.send(speakerAsJson);
+    }).catch(() => {
+        next({
+            message: 'Error',
+            errors: [{message: 'Unable to fetch speaker'}]
+        })
     });
 };
