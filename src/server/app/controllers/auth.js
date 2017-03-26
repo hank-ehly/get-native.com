ffaec82b188c443ed6ceba3ddacfef0a226161b4
Hank Ehly <henry.ehly@gmail.com>
2017-03-26 21:40:28 +0900

    Refactor POST /register method

--- src/server/app/controllers/auth.js
+++ src/server/app/controllers/auth.js
@@ -15,6 +15,7 @@ const Promise    = require('bluebird');
 const mailer     = require('../../config/initializers/mailer');
 const fs         = Promise.promisifyAll(require('fs'));
 const k          = require('../../config/keys.json');
+const _          = require('lodash');
 
 module.exports.login = (req, res, next) => {
     const attributes = [
@@ -40,7 +41,7 @@ module.exports.login = (req, res, next) => {
         AuthHelper.setAuthHeadersOnResponseWithToken(res, token);
         const accountAsJson = account.get({plain: true});
         res.send(accountAsJson);
-    }).catch((e) => {
+    }).catch(() => {
         return next({
             message: 'Authentication Error',
             errors: [{message: 'Account does not exist'}]
@@ -48,12 +49,46 @@ module.exports.login = (req, res, next) => {
     });
 };
 
-module.exports.register = (req, res, next) => {
-    const email    = req.body[k.Attr.Email];
-    const password = req.body[k.Attr.Password];
-    let account    = null;
+function generateWelcomeEmailForRequest(req, callback) {
+    const locale           = req.headers['accept-language'] || 'en';
+    const localeModule     = require(__dirname + '/../../config/locales/' + locale + '.json');
+    const templatesDir     = __dirname + '/../templates/';
+    const textTemplatePath = templatesDir + 'welcome.txt';
+    const htmlTemplatePath = templatesDir + 'welcome.html';
 
+    return Promise.all([fs.readFileAsync(textTemplatePath), fs.readFileAsync(htmlTemplatePath)]).spread((textTemplate, htmlTemplate) => {
+        const templates = _.mapValues({textTemplate: textTemplate, htmlTemplate: htmlTemplate}, (value) => {
+            return _.template(value.toString())({
+                lang: locale,
+                title: localeModule.templates.welcome.title,
+                instructions: localeModule.templates.welcome.instructions,
+                confirmationLinkLabel: localeModule.templates.welcome.confirmationLinkLabel,
+                confirmationLinkUrl: 'https://hankehly.com', // todo: timed expiry link? resend link?
+                footer: localeModule.templates.welcome.footer
+            });
+        });
+
+        callback({
+            from: nconf.get(k.NoReply),
+            to: req.body[k.Attr.Email],
+            subject: localeModule.templates.welcome.title,
+            text: templates.textTemplate,
+            html: templates.htmlTemplate
+        });
+    }).catch((e) => {
+        return next({
+            message: 'Error',
+            errors: [{message: e}]
+        });
+    });
+}
+
+module.exports.register = (req, res, next) => {
+    let account = null;
+
+    // todo: shouldn't this be done by db validations?
     Account.existsForEmail(req.body[k.Attr.Email]).then(exists => {
+        // todo: throw to catch
         if (exists) {
             return next({
                 message: 'Error',
@@ -61,45 +96,43 @@ module.exports.register = (req, res, next) => {
             });
         }
 
-        return Account.create({email: email, password: password})
-    }).then(_account => {
-        account = _account;
-        const htmlTemplate = __dirname + '/../templates/confirm-registration.html';
-        const textTemplate = __dirname + '/../templates/confirm-registration.txt';
-        return Promise.all([fs.readFileAsync(htmlTemplate), fs.readFileAsync(textTemplate)]);
-    }).spread((htmlTemplate, textTemplate) => {
-        const message = {
-            from: nconf.get(k.NoReply),
-            to: email,
-            subject: 'Welcome to Get Native!', // todo: 'translate' helper like Rails
-            text: textTemplate.toString(),
-            html: htmlTemplate.toString()
-        };
-
-        return mailer.sendMail(message).then(() => {
-            return AuthHelper.generateTokenForAccountId(account.id);
+        return Account.create({
+            email: req.body[k.Attr.Email],
+            password: req.body[k.Attr.Password]
         });
+    }).then(_account => {
+        if (!_account) {
+            return next(new Error('Missing account!'));
+        }
 
+        account = _account;
+        generateWelcomeEmailForRequest(req, mail => {
+            return mailer.sendMail(mail);
+        });
+    }).then(() => {
+        return AuthHelper.generateTokenForAccountId(account.id);
     }).then(token => {
         AuthHelper.setAuthHeadersOnResponseWithToken(res, token);
         const accountAsJson = account.get({plain: true});
         res.send(accountAsJson);
     }).catch((e) => {
-        console.log(e);
-
         next({
             message: 'Error',
-            errors: [{message: 'Failed to query existing accounts'}]
+            errors: [{message: e}]
         });
     });
 };
 
 module.exports.authenticate = (req, res, next) => {
     AuthHelper.validateRequest(req, (err, token) => {
-        if (err) return next(err);
+        if (err) {
+            return next(err);
+        }
 
         AuthHelper.refreshToken(token, (err, token) => {
-            if (err) return next(err);
+            if (err) {
+                return next(err);
+            }
 
             AuthHelper.setAuthHeadersOnResponseWithToken(res, token);
             next();
