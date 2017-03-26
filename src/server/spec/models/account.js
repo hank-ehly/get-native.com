1b575d4b9c5f0fbdd695112ea68d08f886c6560c
Hank Ehly <henry.ehly@gmail.com>
2017-03-26 19:30:50 +0900

    Add tests for existsForEmail Account method

---
+++ src/server/spec/models/account.js
@@ -0,0 +1,48 @@
+/**
+ * account
+ * get-native.com
+ *
+ * Created by henryehly on 2017/03/26.
+ */
+
+const assert  = require('assert');
+const SpecUtil = require('../spec-util');
+const Account = require('../../app/models').Account;
+
+describe('Account', function() {
+    let existingAccountEmail      = null;
+    const nonExistentAccountEmail = 'nonexistent@email.com';
+
+    before(function(done) {
+        this.timeout(SpecUtil.defaultTimeout);
+
+        SpecUtil.seedAll(() => {
+            Account.findOne().then(a => {
+                console.log(a.email);
+                existingAccountEmail = a.email;
+                done();
+            });
+        });
+    });
+
+    after(function(done) {
+        this.timeout(SpecUtil.defaultTimeout);
+        SpecUtil.seedAllUndo(done);
+    });
+
+    it(`should return true if a user exists for a given email address`, function() {
+        return Account.existsForEmail(existingAccountEmail).then(exists => {
+            assert(exists);
+        });
+    });
+
+    it(`should return false if a user does not exist for a given email address`, function() {
+        return Account.existsForEmail(nonExistentAccountEmail).then(exists => {
+            assert(!exists);
+        });
+    });
+
+    it(`should throw a TypeError if the first argument is not an email address`, function() {
+        assert.throws(() => Account.existsForEmail(123), TypeError);
+    });
+});
