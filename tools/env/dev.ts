71de67fde66ef642c8f05b021dfcbc097be99f5f
Hank Ehly <henry.ehly@gmail.com>
2017-03-31 14:24:23 +0900

    Allow client to specify API port

--- tools/env/dev.ts
+++ tools/env/dev.ts
@@ -1,7 +1,8 @@
 import { EnvConfig } from './env-config.interface';
+import Config from '../config';
 
 const DevConfig: EnvConfig = {
-    API: 'http://localhost:3000',
+    API: 'http://localhost:' + Config.API_PORT,
     ENV: 'DEV',
     facebookAppId: '215586025582003',
     facebookSDKSrc: '//connect.facebook.net/en_US/sdk/debug.js'
