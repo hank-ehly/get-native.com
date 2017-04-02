55296683f3a8990ae7c448c3c2896b38c37259b4
Hank Ehly <henry.ehly@gmail.com>
2017-04-02 09:10:25 +0900

    Refactor index.js

--- src/server/index.js
+++ src/server/index.js
@@ -6,16 +6,12 @@
  */
 
 const Promise = require('bluebird');
+const nconf   = require('nconf');
 const fs      = require('fs');
 const k       = require('./config/keys.json');
 
-const nconf = require('nconf');
-//noinspection JSUnresolvedFunction
-nconf.use('memory');
-//noinspection JSUnresolvedFunction
-nconf.argv();
-//noinspection JSUnresolvedFunction
 nconf.env();
+nconf.use('memory');
 
 const confPath = __dirname + '/config/environments/';
 
@@ -26,14 +22,14 @@ if (fs.existsSync(envConf)) {
     require(envConf);
 }
 
-const logger   = require('./config/logger');
-const server   = require('./config/initializers/server');
-const database = require('./app/models');
-const mailer   = require('./config/initializers/mailer');
+const db     = require('./app/models');
+const logger = require('./config/logger');
+const server = require('./config/initializers/server');
+const mailer = require('./config/initializers/mailer');
 
 logger.info(`Initializing ${nconf.get(k.Env.Key).toUpperCase()} environment`);
 
-const promises = [server(), database.sequelize.authenticate(), Promise.promisify(mailer.verify)()];
+const promises = [server(), db.sequelize.authenticate(), Promise.promisify(mailer.verify)()];
 
 if (nconf.get(k.Env.Key) === k.Env.Development) {
     const MailDev = require('maildev');
