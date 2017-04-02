7354b2122b844af625f9b965a5fd3bfbaf16575f
Hank Ehly <henry.ehly@gmail.com>
2017-04-02 09:09:17 +0900

    Use keys.json instead of hard-coded values

--- src/server/config/environments/production.js
+++ src/server/config/environments/production.js
@@ -6,6 +6,8 @@
  */
 
 const nconf = require('nconf');
+const k     = require('../keys.json');
 
 nconf.set('allow-origin', 'https://get-native.com');
 nconf.set('hostname', 'api.get-native.com');
+nconf.set(k.NoReply, 'noreply@get-native.com');
