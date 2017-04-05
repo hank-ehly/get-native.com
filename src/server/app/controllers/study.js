0f0ff57bba8c5ea607f52b898b595107672bd40b
Hank Ehly <henry.ehly@gmail.com>
2017-04-05 13:32:13 +0900

    Add mock GET /study/stats/:lang endpoint implementation

--- src/server/app/controllers/study.js
+++ src/server/app/controllers/study.js
@@ -5,18 +5,25 @@
  * Created by henryehly on 2017/01/18.
  */
 
-const db              = require('../models');
-const ModelHelper     = require('../helpers').Model(db);
-const WritingAnswer   = db.WritingAnswer;
-const WritingQuestion = db.WritingQuestion;
 const ResponseWrapper = require('../helpers').ResponseWrapper;
 const AuthHelper      = require('../helpers').Auth;
 const k               = require('../../config/keys.json');
 const logger          = require('../../config/logger');
+const db              = require('../models');
+const ModelHelper     = require('../helpers').Model(db);
+const WritingAnswer   = db.WritingAnswer;
+const WritingQuestion = db.WritingQuestion;
 
 module.exports.stats = (req, res) => {
-    let mock = require('../../mock/study_stats.json');
-    res.send(mock);
+    res.status(200).send({
+        lang: 'en',
+        total_time_studied: 0,
+        consecutive_days: 0,
+        total_study_sessions: 0,
+        longest_consecutive_days: 0,
+        maximum_words: 0,
+        maximum_wpm: 0
+    });
 };
 
 module.exports.writing_answers = (req, res, next) => {
@@ -30,12 +37,5 @@ module.exports.writing_answers = (req, res, next) => {
     }).then(answers => {
         const answersAsJson = ResponseWrapper.wrap(answers.map(a => a.get({plain: true})));
         res.send(answersAsJson);
-    }).catch(e => {
-        logger.info(e, {json: true});
-
-        next({
-            message: 'Error',
-            errors: [{message: e}]
-        });
-    });
+    }).catch(next);
 };
--- src/server/config/routes.js
+++ src/server/config/routes.js
@@ -17,7 +17,7 @@ router.post( '/account/email',         validate(pv.accounts.updateEmail),    ctr
 router.get(  '/categories',            validate(pv.categories.index),        ctrl.auth.authenticate, ctrl.categories.index);
 router.post( '/login',                 validate(pv.auth.login),                                      ctrl.auth.login);
 router.post( '/register',              validate(pv.auth.register),                                   ctrl.auth.register);
-router.get(  '/study/stats',           validate(pv.study.stats),             ctrl.auth.authenticate, ctrl.study.stats);
+router.get(  '/study/stats/:lang',     validate(pv.study.stats),             ctrl.auth.authenticate, ctrl.study.stats);
 router.get(  '/study/writing_answers', validate(pv.study.writing_answers),   ctrl.auth.authenticate, ctrl.study.writing_answers);
 router.get(  '/speakers/:id',          validate(pv.speakers.show),           ctrl.auth.authenticate, ctrl.speakers.show);
 router.get(  '/videos',                validate(pv.videos.index),            ctrl.auth.authenticate, ctrl.videos.index);
