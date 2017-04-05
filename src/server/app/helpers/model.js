a7d7a08b9341d57cfdfa4bedc5b34ac055f9c700
Hank Ehly <henry.ehly@gmail.com>
2017-04-06 08:17:30 +0900

    Format code

--- src/server/app/helpers/model.js
+++ src/server/app/helpers/model.js
@@ -13,8 +13,8 @@ module.exports = (db) => {
         const fn  = db.sequelize.fn;
         const col = db.sequelize.col([table, column].join('.'));
 
-        const UTCOffset    = '+00:00';
-        const clientOffset = Utility.browserTimezoneOffsetToSQLFormat(timeZoneOffset);
+        const UTCOffset     = '+00:00';
+        const clientOffset  = Utility.browserTimezoneOffsetToSQLFormat(timeZoneOffset);
         const offsetDateCol = fn('CONVERT_TZ', col, UTCOffset, clientOffset);
 
         const dateHead   = fn('DATE_FORMAT', offsetDateCol, '%a %b %d %H:%i:%S ');
