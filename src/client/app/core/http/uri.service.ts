44353e76dcb69773a231932c3d3b773d4f1391c2
Hank Ehly <henry.ehly@gmail.com>
2017-04-14 20:54:59 +0900

    Refactor uri.service to not mutate original endpoint url

--- src/client/app/core/http/uri.service.ts
+++ src/client/app/core/http/uri.service.ts
@@ -9,29 +9,32 @@ import { Injectable } from '@angular/core';
 
 import { Logger } from '../logger/logger';
 
+import * as _ from 'lodash';
+
 @Injectable()
 export class URIService {
     constructor(private logger: Logger) {
     }
 
-    generateURIForEndpointWithParams(urlParams: any, endpoint: any): string {
+    generateURIForEndpointWithParams(params: any, endpoint: any): string {
         let matches = endpoint.url.match(/:[a-z]+/g);
 
         if (!matches) {
             return endpoint.url;
         }
 
-        this.logger.debug(this, matches);
+        let url = endpoint.url;
 
-        for (let match of matches) {
+        _.forIn(matches, match => {
             let key = match.substr(1);
-            if (urlParams[key]) {
-                endpoint.url = endpoint.url.replace(match, urlParams[key]);
+
+            if (params[key]) {
+                url = _.replace(endpoint.url, match, params[key]);
             } else {
                 throw new Error(`Could not find value for key '${key}' in requestArgs.`);
             }
-        }
+        });
 
-        return endpoint.url;
+        return url;
     }
 }
