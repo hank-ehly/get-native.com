6ae5c68f19e57cdd7d60f47853c7d60562321326
Hank Ehly <henry.ehly@gmail.com>
2017-04-16 18:10:27 +0900

    Add more realistic date seed-data to videos

--- src/server/db/seeders/20170227213503-videos.js
+++ src/server/db/seeders/20170227213503-videos.js
@@ -10,6 +10,7 @@ const Speaker     = models.Speaker;
 const Subcategory = models.Subcategory;
 const chance      = require('chance').Chance();
 const Promise     = require('bluebird');
+const moment      = require('moment');
 
 module.exports = {
     up: function(queryInterface, Sequelize) {
@@ -20,7 +21,19 @@ module.exports = {
             const numVideos       = 500;
             const youtubeVideoIds = ['SqyDRXVd5Jo', 'clpOP8f3Jc8', 'q9k_QgYA-bo', 'rF-MsURy9q8', 'W2G68H3xRyE'];
 
+            const minDate         = moment().subtract(numVideos + 10, 'days');
+
             for (let i = 0; i < numVideos; i++) {
+                let created_at   = moment(minDate).add(i + 1, 'days');
+                let updated_at   = chance.pickone([
+                    moment(created_at),
+                    moment(created_at).add(1, 'days'),
+                    moment(created_at).add(2, 'days'),
+                    moment(created_at).add(3, 'days'),
+                    moment(created_at).add(4, 'days'),
+                    moment(created_at).add(5, 'days')
+                ]);
+
                 videos.push({
                     length: chance.integer({
                         min: 30,
@@ -41,7 +54,9 @@ module.exports = {
                     subcategory_id: chance.integer({
                         min: minSubcategoryId,
                         max: maxSubcategoryId
-                    })
+                    }),
+                    created_at: created_at.toDate(),
+                    updated_at: updated_at.toDate()
                 });
             }
 
