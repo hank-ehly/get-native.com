c90b6c7780df80327a3c48e21faeb67e6a1d0c04
Hank Ehly <henry.ehly@gmail.com>
2017-04-17 20:59:48 +0900

    Correct account_activations table/model naming

--- src/server/app/models/account-activation.js
+++ src/server/app/models/account-activation.js
@@ -9,7 +9,7 @@ const Utility = require('../helpers').Utility;
 const _       = require('lodash');
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('Account', {
+    return sequelize.define('AccountActivation', {
         id: {
             allowNull: false,
             autoIncrement: true,
@@ -26,7 +26,7 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.DATE
         }
     }, {
-        tableName: 'accounts',
+        tableName: 'account_activations',
         underscored: true,
         associations: function(models) {
             models.AccountActivation.belongsTo(models.Account);
