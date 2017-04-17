f361a5fcea4d6c9a96247c1b915d7f373e471c35
Hank Ehly <henry.ehly@gmail.com>
2017-04-18 07:45:19 +0900

    Rename AccountActivation to VerificationToken

--- src/server/app/models/verification-token.js
+++ src/server/app/models/verification-token.js
@@ -1,5 +1,5 @@
 /**
- * account
+ * verification-token
  * get-native.com
  *
  * Created by henryehly on 2017/02/24.
@@ -9,14 +9,14 @@ const Utility = require('../helpers').Utility;
 const _       = require('lodash');
 
 module.exports = function(sequelize, DataTypes) {
-    return sequelize.define('AccountActivation', {
+    return sequelize.define('VerificationToken', {
         id: {
             allowNull: false,
             autoIncrement: true,
             primaryKey: true,
             type: DataTypes.INTEGER
         },
-        code: {
+        token: {
             type: DataTypes.STRING,
             allowNull: false,
             defaultValue: ''
@@ -26,10 +26,10 @@ module.exports = function(sequelize, DataTypes) {
             type: DataTypes.DATE
         }
     }, {
-        tableName: 'account_activations',
+        tableName: 'verification_tokens',
         underscored: true,
         associations: function(models) {
-            models.AccountActivation.belongsTo(models.Account);
+            models.VerificationToken.belongsTo(models.Account);
         }
     });
 };
