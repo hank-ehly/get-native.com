8db20a15821d5cd82afd504ae210cef9558b8ea0
Hank Ehly <henry.ehly@gmail.com>
2017-04-20 11:39:33 +0900

    Refactor response-wrapper service to use lodash

--- src/server/app/services/response-wrapper.js
+++ src/server/app/services/response-wrapper.js
@@ -5,55 +5,50 @@
  * Created by henryehly on 2017/03/14.
  */
 
-const util = require('./utility');
+const _ = require('lodash');
 
-module.exports = {
-    wrap(list) {
-        let listType = util.typeof(list);
-        if (listType !== 'array') {
-            throw new TypeError(`Argument must be an array. Received: ${listType}`);
-        }
-
-        let wrappedList = {records: [], count: 0};
-
-        for (let item of list) {
-            let itemType = util.typeof(item);
-            if (itemType !== 'object') {
-                throw new TypeError(`Item must be an object. Received: ${itemType}`);
-            }
-
-            wrappedList.records.push(item);
-            wrappedList.count += 1;
-        }
-
-        return wrappedList;
-    },
-
-    deepWrap(list, properties) {
-        if (arguments.length < 2) {
-            throw new Error('No sub-property list provided. Please use \'wrap\' instead.');
-        }
-
-        let propertiesType = util.typeof(properties);
-        if (propertiesType !== 'array') {
-            throw new TypeError(`Sub-property list must be of type 'array'. Received '${propertiesType}'`);
-        }
-
-        let wrappedList = this.wrap(list);
-
-        for (let p of properties) {
-
-            for (let i = 0; i < wrappedList.count; i++) {
-                let record = wrappedList.records[i];
-
-                if (!record.hasOwnProperty(p)) {
-                    throw new ReferenceError(`Record has no own property '${p}'`);
-                }
-
-                record[p] = this.wrap(record[p]);
-            }
-        }
-
-        return wrappedList;
+module.exports.wrap = (list) => {
+    if (!_.isArray(list)) {
+        throw new TypeError(`Argument must be an array`);
     }
+
+    let wrappedList = {records: [], count: 0};
+
+    for (let item of list) {
+        if (!_.isPlainObject(item)) {
+            throw new TypeError(`Item must be an object`);
+        }
+
+        wrappedList.records.push(item);
+        wrappedList.count += 1;
+    }
+
+    return wrappedList;
+};
+
+module.exports.deepWrap = (list, properties) => {
+    if (arguments.length < 2) {
+        throw new Error('No sub-property list provided. Please use \'wrap\' instead.');
+    }
+
+    if (!_.isArray(properties)) {
+        throw new TypeError(`Sub-property list must be of type 'array'`);
+    }
+
+    let wrappedList = module.exports.wrap(list);
+
+    for (let p of properties) {
+
+        for (let i = 0; i < wrappedList.count; i++) {
+            let record = wrappedList.records[i];
+
+            if (!record.hasOwnProperty(p)) {
+                throw new ReferenceError(`Record has no own property '${p}'`);
+            }
+
+            record[p] = module.exports.wrap(record[p]);
+        }
+    }
+
+    return wrappedList;
 };
