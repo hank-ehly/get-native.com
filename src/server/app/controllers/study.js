5cb69ae5f8010306e810659ecd54c193898f0e3c
Hank Ehly <henry.ehly@gmail.com>
2017-04-21 10:44:01 +0900

    Use Promise.join

--- src/server/app/controllers/study.js
+++ src/server/app/controllers/study.js
@@ -19,22 +19,22 @@ const Promise         = require('bluebird');
 module.exports.stats = (req, res, next) => {
     const accountId = Auth.extractAccountIdFromRequest(req);
 
-    Account.findById(accountId).then(a => {
-        return Promise.all([
-            a.calculateStudySessionStatsForLanguage(req.params.lang),
-            a.calculateWritingStatsForLanguage(req.params.lang),
-            a.calculateStudyStreaksForLanguage(req.params.lang)
-        ]);
-    }).spread((studySessionStats, writingStats, studyStreakStats) => {
-        res.status(200).send({
-            lang: req.params.lang,
-            total_time_studied: studySessionStats.total_time_studied,
-            consecutive_days: studyStreakStats.consecutive_days,
-            total_study_sessions: studySessionStats.total_study_sessions,
-            longest_consecutive_days: studyStreakStats.longest_consecutive_days,
-            maximum_words: writingStats.maximum_words,
-            maximum_wpm: writingStats.maximum_wpm
-        });
+    Account.findById(accountId).then(account => {
+        const sessionStats = account.calculateStudySessionStatsForLanguage(req.params.lang);
+        const writingStats = account.calculateWritingStatsForLanguage(req.params.lang);
+        const studyStreaks = account.calculateStudyStreaksForLanguage(req.params.lang);
+
+        return Promise.join(sessionStats, writingStats, studyStreaks, (sessions, writing, streaks) => {
+            res.status(200).send({
+                lang: req.params.lang,
+                total_time_studied: sessions.total_time_studied,
+                consecutive_days: streaks.consecutive_days,
+                total_study_sessions: sessions.total_study_sessions,
+                longest_consecutive_days: streaks.longest_consecutive_days,
+                maximum_words: writing.maximum_words,
+                maximum_wpm: writing.maximum_wpm
+            });
+        }).catch(next);
     }).catch(next);
 };
 
@@ -43,7 +43,10 @@ module.exports.writing_answers = (req, res, next) => {
     const createdAt = ModelService.getDateAttrForTableColumnTZOffset(k.Model.WritingAnswer, k.Attr.CreatedAt, req.query.time_zone_offset);
 
     WritingAnswer.scope([
-        'newestFirst', {method: ['forAccountWithLang', accountId, req.params.lang]}, {method: ['since', req.query.since]}, {method: ['maxId', req.query.max_id]}
+        'newestFirst',
+        {method: ['forAccountWithLang', accountId, req.params.lang]},
+        {method: ['since', req.query.since]},
+        {method: ['maxId', req.query.max_id]}
     ]).findAll({
         attributes: [k.Attr.Id, k.Attr.Answer, createdAt, k.Attr.StudySessionId],
         include: [
