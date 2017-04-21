42372dbb64bff70963f9fd2c4253325f7ab598ca
Hank Ehly <henry.ehly@gmail.com>
2017-04-21 19:25:24 +0900

    Refactor user service

--- src/client/app/core/user/user.service.ts
+++ src/client/app/core/user/user.service.ts
@@ -11,44 +11,53 @@ import { User } from '../entities/user';
 import { Language } from '../typings/language';
 import { LangService } from '../lang/lang.service';
 import { LocalStorageService } from '../local-storage/local-storage.service';
-import { kCurrentUser, kAuthToken, kAuthTokenExpire } from '../local-storage/local-storage-keys';
+import { kCurrentUser, kAuthToken, kAuthTokenExpire, kAcceptLocalStorage } from '../local-storage/local-storage-keys';
 import { Logger } from '../logger/logger';
 
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Subject } from 'rxjs/Subject';
-import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/mapTo';
 import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/do';
 import * as _ from 'lodash';
 
 @Injectable()
 export class UserService {
     currentStudyLanguage$ = new ReplaySubject<Language>(1);
-    current$              = new BehaviorSubject<User>(this.localStorage.getItem(kCurrentUser));
-
+    authenticated$        = new BehaviorSubject<boolean>(false);
     logout$               = new Subject<any>();
 
-    private onUpdate$ = this.current$.filter((u: User) => !_.isEmpty(u));
+    current$ = new BehaviorSubject<User>(this.localStorage.getItem(kCurrentUser));
 
     constructor(private lang: LangService, private localStorage: LocalStorageService, private logger: Logger) {
-        this.onUpdate$
-            .do(u => this.localStorage.setItem(kCurrentUser, u))
-            .map(u => this.lang.languageForCode(u.default_study_language_code))
+        this.current$
+            .filter(_.isObject)
+            .do((u: User) => this.logger.debug(this, u))
+            .do(this.onUpdateUser.bind(this))
+            .map((u: User) => this.lang.languageForCode(u.default_study_language_code))
             .subscribe(this.currentStudyLanguage$);
 
-        this.logout$.subscribe(this.logout.bind(this));
+        this.current$
+            .filter(_.isObject)
+            .mapTo(true)
+            .subscribe(this.authenticated$);
+
+        this.logout$
+            .do((user: User) => this.logger.debug(this, 'logout'))
+            .do(this.onLogout.bind(this))
+            .mapTo(false)
+            .subscribe(this.authenticated$);
     }
 
-    isLoggedIn(): boolean {
-        if (!this.localStorage.hasItem(kAuthToken)) {
-            return false;
-        }
+    private onUpdateUser(user: User): void {
+        this.localStorage.setItem(kCurrentUser, user);
 
-        return this.localStorage.hasItem(kAuthTokenExpire) && +this.localStorage.getItem(kAuthTokenExpire) > Date.now();
+        // this only needs to be done once
+        this.localStorage.setItem(kAcceptLocalStorage, true);
     }
 
-    private logout(): void {
-        this.logger.debug(this, 'onLogout');
+    private onLogout(): void {
         this.localStorage.removeItem(kAuthToken);
         this.localStorage.removeItem(kAuthTokenExpire);
         this.localStorage.removeItem(kCurrentUser);
