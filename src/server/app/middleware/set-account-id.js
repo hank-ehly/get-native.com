a03b021b08ddfb38b1c6c289d5e3e816001f120b
Hank Ehly <henry.ehly@gmail.com>
2017-04-21 13:57:02 +0900

    Fix confirm_email server-side logic

--- src/server/app/middleware/set-account-id.js
+++ src/server/app/middleware/set-account-id.js
@@ -5,14 +5,27 @@
  * Created by henryehly on 2017/04/21.
  */
 
-const Auth = require('../services').Auth;
+const logger         = require('../../config/logger');
+const services       = require('../services');
+const GetNativeError = services.GetNativeError;
+const Auth           = services.Auth;
+const k              = require('../../config/keys.json');
 
 const _    = require('lodash');
 
 module.exports = (req, res, next) => {
     if (req.headers && req.headers.authorization) {
+        const accountId = Auth.extractAccountIdFromRequest(req);
+
+        logger.info('Account ID:', accountId);
+
+        if (!accountId) {
+            res.status(422);
+            next(new GetNativeError(k.Error.JWT));
+        }
+
         _.assign(req, {
-            accountId: Auth.extractAccountIdFromRequest(req)
+            accountId: accountId
         });
     }
     next();
