a2d0d862a3d030900534dd7273fd6aee916b244d
Hank Ehly <henry.ehly@gmail.com>
2017-04-23 13:54:19 +0900

    Refactor notifiaction enable/disable logic

--- src/client/app/settings/notifications/notifications.component.ts
+++ src/client/app/settings/notifications/notifications.component.ts
@@ -5,16 +5,10 @@
  * Created by henryehly on 2016/12/09.
  */
 
-import { Component, ViewChild, OnDestroy } from '@angular/core';
+import { Component } from '@angular/core';
 
-import { SwitchComponent } from '../../shared/switch/switch.component';
-import { Logger } from '../../core/logger/logger';
-import { HttpService } from '../../core/http/http.service';
 import { UserService } from '../../core/user/user.service';
-import { APIHandle } from '../../core/http/api-handle';
-
-import { Subscription } from 'rxjs/Subscription';
-import * as _ from 'lodash';
+import { User } from '../../core/entities/user';
 
 @Component({
     moduleId: module.id,
@@ -22,31 +16,21 @@ import * as _ from 'lodash';
     templateUrl: 'notifications.component.html',
     styleUrls: ['notifications.component.css']
 })
-export class NotificationsComponent implements OnDestroy {
-    @ViewChild('emailSwitch') emailSwitch: SwitchComponent;
-    @ViewChild('browserSwitch') browserSwitch: SwitchComponent;
+export class NotificationsComponent {
+    user: User = this.userService.current$.getValue();
 
-    private subscriptions: Subscription[] = [];
-
-    constructor(private logger: Logger, private http: HttpService, public user: UserService) {
+    constructor(private userService: UserService) {
     }
 
-    ngOnDestroy(): void {
-        this.logger.debug(this, 'OnDestroy');
-        _.forEach(this.subscriptions, s => s.unsubscribe());
+    updateEmailPreference(value: boolean) {
+        this.userService.update({email_notifications_enabled: value});
     }
 
-    onToggleEmailNotifications(value: boolean): void {
-        this.logger.debug(this, `onToggleEmailNotifications(${value})`);
-        this.subscriptions.push(
-            this.http.request(APIHandle.EDIT_ACCOUNT, {body: {email_notifications_enabled: value}}).subscribe()
-        );
+    updateBrowserPreference(value: boolean) {
+        this.userService.update({browser_notifications_enabled: value});
     }
 
-    onToggleBrowserNotifications(value: boolean): void {
-        this.logger.debug(this, `onToggleBrowserNotifications(${value})`);
-        this.subscriptions.push(
-            this.http.request(APIHandle.EDIT_ACCOUNT, {body: {browser_notifications_enabled: value}}).subscribe()
-        );
+    isEmailVerificationsOn(): boolean {
+        return this.user.email_verified ? this.user.email_notifications_enabled : false;
     }
 }
