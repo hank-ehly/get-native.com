303efa43736a75f252abff8fac8331b1b2d51f1f
Hank Ehly <henry.ehly@gmail.com>
2017-04-24 17:39:39 +0900

    Update the email sender settings (for once and for all!)

--- src/server/config/environments/staging.js
+++ src/server/config/environments/staging.js
@@ -12,8 +12,8 @@ const config = {};
 config[k.Header.AccessControlAllowOrigin] = 'https://stg.get-native.com';
 config[k.API.Hostname] = 'stg.api.get-native.com';
 config[k.SMTP.Port] = 25;
-config[k.SMTP.Host] = 'stg.get-native.com';
 config[k.Client.Host] = 'stg.get-native.com';
 config[k.Client.Protocol] = 'https';
+config[k.NoReply] = 'noreply@stg.get-native.com';
 
 module.exports = config;
