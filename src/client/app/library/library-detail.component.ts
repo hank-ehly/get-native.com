329b8c0e20391578df8a6ea56d9cdc72b1b1c7b1
Hank Ehly <henry.ehly@gmail.com>
2017-04-27 16:30:24 -0500

    Fix 'liked' logic in library detail template

--- src/client/app/library/library-detail.component.ts
+++ src/client/app/library/library-detail.component.ts
@@ -14,17 +14,16 @@ import { NavbarService } from '../core/navbar/navbar.service';
 import { HttpService } from '../core/http/http.service';
 import { APIHandle } from '../core/http/api-handle';
 
-import { BehaviorSubject } from 'rxjs/BehaviorSubject';
-import { NextObserver } from 'rxjs/Observer';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/debounceTime';
-import 'rxjs/add/operator/concatMap';
+import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/share';
 import 'rxjs/add/operator/pluck';
 
 import * as _ from 'lodash';
+import { Subscription } from 'rxjs';
 
 @Component({
     moduleId: module.id,
@@ -35,9 +34,12 @@ import * as _ from 'lodash';
 export class LibraryDetailComponent implements OnInit, OnDestroy {
     video$: Observable<Video>;
 
-    liked$ = new BehaviorSubject<boolean>(null);
+    setLiked$ = new Subject<boolean>();
+    liked: boolean;
 
     likeCount: number;
+
+    subscriptions: Subscription[] = [];
 
     reqOptions = {
         params: {
@@ -53,27 +55,31 @@ export class LibraryDetailComponent implements OnInit, OnDestroy {
 
         this.video$ = this.http.request(APIHandle.VIDEO, this.reqOptions).share().do((v: Video) => {
             this.likeCount = v.like_count;
+            this.liked = v.liked;
         });
 
-        this.video$.pluck('subcategory', 'name').subscribe((t: string) => {
-            this.navbar.title$.next(t);
-        });
+        this.subscriptions.push(
+            this.video$.pluck('subcategory', 'name').subscribe((t: string) => this.navbar.title$.next(t)),
 
-        this.liked$.filter(_.isBoolean).do(this.updateLikeCount.bind(this)).debounceTime(300).distinctUntilChanged().subscribe(
-            <NextObserver<boolean>>this.onLikedChange.bind(this)
+            this.setLiked$.filter(_.isBoolean)
+                .do(this.updateLikeCount.bind(this))
+                .do(this.updateLiked.bind(this))
+                .debounceTime(300).distinctUntilChanged().mergeMap(liked => {
+                return this.http.request(liked ? APIHandle.LIKE_VIDEO : APIHandle.UNLIKE_VIDEO, this.reqOptions);
+            }).subscribe()
         );
     }
 
     ngOnDestroy(): void {
         this.logger.debug(this, 'OnDestroy');
-    }
-
-    private onLikedChange(liked: boolean) {
-        this.logger.debug(this, 'liked change', liked);
-        this.http.request(liked ? APIHandle.LIKE_VIDEO : APIHandle.UNLIKE_VIDEO, this.reqOptions);
+        _.each(this.subscriptions, s => s.unsubscribe());
     }
 
     private updateLikeCount(liked: boolean) {
         this.likeCount = liked ? this.likeCount + 1 : this.likeCount - 1;
+    }
+
+    private updateLiked(liked: boolean) {
+        this.liked = liked;
     }
 }
