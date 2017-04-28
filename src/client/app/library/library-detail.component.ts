eb437e06644d03696e5a73c843be44de2e1b599f
Hank Ehly <henry.ehly@gmail.com>
2017-04-28 12:24:42 -0500

    Display more navbar items in library detail component

--- src/client/app/library/library-detail.component.ts
+++ src/client/app/library/library-detail.component.ts
@@ -14,16 +14,17 @@ import { NavbarService } from '../core/navbar/navbar.service';
 import { HttpService } from '../core/http/http.service';
 import { APIHandle } from '../core/http/api-handle';
 
+import { Subscription } from 'rxjs/Subscription';
 import { Observable } from 'rxjs/Observable';
 import { Subject } from 'rxjs/Subject';
 import 'rxjs/add/operator/distinctUntilChanged';
 import 'rxjs/add/operator/debounceTime';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/mergeMap';
 import 'rxjs/add/operator/share';
 import 'rxjs/add/operator/pluck';
 
 import * as _ from 'lodash';
-import { Subscription } from 'rxjs';
 
 @Component({
     moduleId: module.id,
@@ -32,20 +33,23 @@ import { Subscription } from 'rxjs';
     styleUrls: ['library-detail.component.css']
 })
 export class LibraryDetailComponent implements OnInit, OnDestroy {
-    video$: Observable<Video>;
-
-    setLiked$ = new Subject<boolean>();
+    likedChange$ = new Subject<boolean>();
     liked: boolean;
 
     likeCount: number;
 
     subscriptions: Subscription[] = [];
 
-    reqOptions = {
-        params: {
-            id: +this.route.snapshot.params['id']
-        }
-    };
+    video$ = this.route.params.pluck('id').map(_.toNumber).switchMap(id => {
+        return this.http.request(APIHandle.VIDEO, {
+            params: {
+                id: id
+            }
+        });
+    }).share().do((v: Video) => {
+        this.likeCount = v.like_count;
+        this.liked = v.liked;
+    });
 
     constructor(private logger: Logger, private navbar: NavbarService, private http: HttpService, private route: ActivatedRoute) {
     }
@@ -53,25 +57,31 @@ export class LibraryDetailComponent implements OnInit, OnDestroy {
     ngOnInit() {
         this.logger.debug(this, 'OnInit');
 
-        this.video$ = this.http.request(APIHandle.VIDEO, this.reqOptions).share().do((v: Video) => {
-            this.likeCount = v.like_count;
-            this.liked = v.liked;
-        });
+        this.navbar.backButtonTitle$.next('Back to Library');
+        this.navbar.studyOptionsVisible$.next(true);
 
         this.subscriptions.push(
             this.video$.pluck('subcategory', 'name').subscribe((t: string) => this.navbar.title$.next(t)),
 
-            this.setLiked$.filter(_.isBoolean)
+            this.likedChange$.filter(_.isBoolean)
                 .do(this.updateLikeCount.bind(this))
                 .do(this.updateLiked.bind(this))
                 .debounceTime(300).distinctUntilChanged().mergeMap(liked => {
-                return this.http.request(liked ? APIHandle.LIKE_VIDEO : APIHandle.UNLIKE_VIDEO, this.reqOptions);
+                return this.http.request(liked ? APIHandle.LIKE_VIDEO : APIHandle.UNLIKE_VIDEO, {
+                    params: {
+                        id: _.toNumber(this.route.snapshot.params['id'])
+                    }
+                });
             }).subscribe()
         );
     }
 
     ngOnDestroy(): void {
         this.logger.debug(this, 'OnDestroy');
+
+        this.navbar.studyOptionsVisible$.next(false);
+        this.navbar.backButtonTitle$.next(null);
+
         _.each(this.subscriptions, s => s.unsubscribe());
     }
 
