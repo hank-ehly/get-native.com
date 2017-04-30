be2859f927db54ce337e9ce1603acd9a6f372856
Hank Ehly <henry.ehly@gmail.com>
2017-04-30 18:44:31 -0500

    Add OnDestroy hook to StudyProgressComponent for debug log purposes

--- src/client/app/shared/study-progress/study-progress.component.ts
+++ src/client/app/shared/study-progress/study-progress.component.ts
@@ -5,7 +5,7 @@
  * Created by henryehly on 2016/12/13.
  */
 
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input, OnDestroy, OnInit } from '@angular/core';
 
 import { Logger } from '../../core/logger/logger';
 
@@ -17,7 +17,7 @@ import { Observable } from 'rxjs/Observable';
     templateUrl: 'study-progress.component.html',
     styleUrls: ['study-progress.component.css']
 })
-export class StudyProgressComponent implements OnInit {
+export class StudyProgressComponent implements OnInit, OnDestroy {
     @Input() progress: {
         listening$: Observable<number>,
         shadowing$: Observable<number>,
@@ -28,7 +28,11 @@ export class StudyProgressComponent implements OnInit {
     constructor(private logger: Logger) {
     }
 
-    ngOnInit() {
+    ngOnInit(): void {
         this.logger.debug(this, 'OnInit');
+    }
+
+    ngOnDestroy(): void {
+        this.logger.debug(this, 'OnDestroy');
     }
 }
