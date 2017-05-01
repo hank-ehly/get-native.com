64864be9d14d0bb3a6df0abec975837bfef87a24
Hank Ehly <henry.ehly@gmail.com>
2017-05-01 13:42:55 -0500

    Allow user to exit study session

--- src/client/app/study/speaking/speaking.component.ts
+++ src/client/app/study/speaking/speaking.component.ts
@@ -8,8 +8,12 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { StudySessionService } from '../../core/study-session/study-session.service';
+import { kWriting } from '../../core/study-session/section-keys';
 import { Transcripts } from '../../core/entities/transcripts';
 import { Logger } from '../../core/logger/logger';
+
+import { Subscription } from 'rxjs/Subscription';
+import * as _ from 'lodash';
 
 @Component({
     moduleId: module.id,
@@ -17,16 +21,24 @@ import { Logger } from '../../core/logger/logger';
     styleUrls: ['speaking.component.css']
 })
 export class SpeakingComponent implements OnInit, OnDestroy {
-    transcripts: Transcripts = this.studySession.current.video.transcripts;
+    transcripts: Transcripts = this.session.current.video.transcripts;
 
-    constructor(private logger: Logger, private studySession: StudySessionService) {
+    subscriptions: Subscription[] = [];
+
+    constructor(private logger: Logger, private session: StudySessionService) {
     }
 
-    ngOnInit() {
+    ngOnInit(): void {
         this.logger.debug(this, 'OnInit');
+
+        this.subscriptions.push(
+            this.session.progressEmitted$.subscribe(this.session.progress.speakingEmitted$),
+            this.session.progressEmitted$.subscribe(null, null, () => this.session.transition(kWriting))
+        );
     }
 
     ngOnDestroy(): void {
         this.logger.debug(this, 'OnDestroy');
+        _.each(this.subscriptions, s => s.unsubscribe());
     }
 }
--- src/client/app/study/transition/transition.component.ts
+++ src/client/app/study/transition/transition.component.ts
@@ -10,13 +10,15 @@ import { Router } from '@angular/router';
 
 import { kListening, kShadowing, kSpeaking, kWriting } from '../../core/study-session/section-keys';
 import { StudySessionService } from '../../core/study-session/study-session.service';
-import { NavbarService } from '../../core/navbar/navbar.service';
 import { Logger } from '../../core/logger/logger';
 
-import { IntervalObservable } from 'rxjs/observable/IntervalObservable';
+import { TimerObservable } from 'rxjs/observable/TimerObservable';
 import { BehaviorSubject } from 'rxjs/BehaviorSubject';
+import { Subscription } from 'rxjs/Subscription';
+import 'rxjs/add/operator/switchMap';
 import 'rxjs/add/operator/switch';
 import 'rxjs/add/operator/take';
+import 'rxjs/observable/never';
 import * as _ from 'lodash';
 
 @Component({
@@ -26,27 +28,29 @@ import * as _ from 'lodash';
     styleUrls: ['transition.component.css']
 })
 export class TransitionComponent implements OnInit, OnDestroy {
-    transitionTimer  = IntervalObservable.create(1000).take(4);
+    transitionTimer  = TimerObservable.create(0, 1000).take(4);
     exercise         = _.toUpper(this.session.current.section);
     count$           = new BehaviorSubject<number>(3);
 
-    constructor(private logger: Logger, private router: Router, private navbar: NavbarService, private session: StudySessionService) {
+    subscriptions: Subscription[] = [];
+
+    constructor(private logger: Logger, private router: Router, private session: StudySessionService) {
     }
 
     ngOnInit(): void {
         this.logger.debug(this, 'OnInit');
-        this.transitionTimer.subscribe(this.onNext.bind(this), null, this.onComplete.bind(this));
+        this.subscriptions.push(this.transitionTimer.subscribe(this.onNext.bind(this), null, this.onComplete.bind(this)));
         this.session.resetCountdown();
     }
 
     ngOnDestroy(): void {
         this.logger.debug(this, 'OnDestroy');
+        _.each(this.subscriptions, s => s.unsubscribe());
     }
 
     onNext(x: number): void {
         this.logger.debug(this, 'onNext', x);
         const nextCount = this.count$.getValue() - 1;
-
         if (_.gte(nextCount, 0)) {
             this.count$.next(nextCount);
         }
@@ -54,29 +58,7 @@ export class TransitionComponent implements OnInit, OnDestroy {
 
     onComplete(): void {
         this.logger.debug(this, 'onComplete');
-        const path = '/study/' + this.session.current.section;
-        this.router.navigate([path]).then(() => {
-            this.logger.debug(this, `navigated to ${path}`);
-            switch (this.session.current.section) {
-                case kListening:
-                    this.session.progressEmitted$.subscribe(this.session.progress.listeningEmitted$);
-                    this.session.progressEmitted$.subscribe(null, null, () => this.session.transition(kShadowing));
-                    break;
-                case kShadowing:
-                    this.session.progressEmitted$.subscribe(this.session.progress.shadowingEmitted$);
-                    this.session.progressEmitted$.subscribe(null, null, () => this.session.transition(kSpeaking));
-                    break;
-                case kSpeaking:
-                    this.session.progressEmitted$.subscribe(this.session.progress.speakingEmitted$);
-                    this.session.progressEmitted$.subscribe(null, null, () => this.session.transition(kWriting));
-                    break;
-                case kWriting:
-                    this.session.progressEmitted$.subscribe(this.session.progress.writingEmitted$);
-                    this.session.progressEmitted$.subscribe(null, null, () => this.router.navigate(['/study/results']));
-                    break;
-                default:
-                    break;
-            }
+        this.router.navigate(['/study/' + this.session.current.section]).then(() => {
             this.session.startCountdown();
         });
     }
