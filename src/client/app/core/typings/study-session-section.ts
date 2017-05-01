8adf46cd2f462ff3aa973dc07c2900ddab9eaa7f
Hank Ehly <henry.ehly@gmail.com>
2017-05-01 09:58:22 -0500

    Move study-session-section typing to typings folder

---
+++ src/client/app/core/typings/study-session-section.ts
@@ -0,0 +1,8 @@
+/**
+ * study-session-section
+ * get-native.com
+ *
+ * Created by henryehly on 2017/05/01.
+ */
+
+export type StudySessionSection = 'listening' | 'shadowing' | 'speaking' | 'writing';
