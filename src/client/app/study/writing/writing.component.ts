730a3f169a476847e54e923efd9919d026124e9b
Hank Ehly <henry.ehly@gmail.com>
2017-05-04 09:57:29 -0500

    Add basic word count functionality to writing component

--- src/client/app/study/writing/writing.component.ts
+++ src/client/app/study/writing/writing.component.ts
@@ -9,13 +9,14 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { StudySessionService } from '../../core/study-session/study-session.service';
-import { Collocations } from '../../core/entities/collocations';
 import { Transcript } from '../../core/entities/transcript';
 import { Logger } from '../../core/logger/logger';
 
 import { Subscription } from 'rxjs/Subscription';
+import { Observable } from 'rxjs/Observable';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/startWith';
 import * as _ from 'lodash';
-import { Transcripts } from '../../core/entities/transcripts';
 
 @Component({
     moduleId: module.id,
@@ -23,22 +24,22 @@ import { Transcripts } from '../../core/entities/transcripts';
     styleUrls: ['writing.component.css']
 })
 export class WritingComponent implements OnInit, OnDestroy {
-    // todo: Make sure you're getting the right language
-    collocations: Collocations;
+    transcript: Transcript;
 
     questionText$  = this.route.data.pluck('question', 'text');
     exampleAnswer$ = this.route.data.pluck('question', 'example_answer');
 
+    wordCount$: Observable<number>;
+    private emitWordCount = new Subject<number>();
+
     private subscriptions: Subscription[] = [];
 
     constructor(private logger: Logger, private session: StudySessionService, private router: Router, private route: ActivatedRoute) {
-        const transcript: Transcript = _.find(this.session.current.video.transcripts.records, {
+        this.transcript = _.find(this.session.current.video.transcripts.records, {
             language_code: this.session.current.video.language_code
         });
 
-        this.logger.debug(this, 'transcript', transcript);
-
-        this.collocations = transcript.collocations;
+        this.wordCount$ = this.emitWordCount.startWith(0);
     }
 
     ngOnInit(): void {
@@ -50,8 +51,27 @@ export class WritingComponent implements OnInit, OnDestroy {
         );
     }
 
+
     ngOnDestroy(): void {
         this.logger.debug(this, 'OnDestroy');
         _.each(this.subscriptions, s => s.unsubscribe());
+    }
+
+    // todo: what to exclude for different languages
+    // todo: characters like じゃ should be 1 word
+    onInput(e: Event): void {
+        const s = (<HTMLTextAreaElement>e.target).value;
+        let value: string = _.replace(s, /[.,\/#!$%\^&\*;:{}=\-_`~()、。「」『』！＠＃＄％＾＆＊（）：”’？＞＜＋＿｜]/g, '');
+
+        let wordRegex = /[\S]+/g;
+
+        if (this.transcript.language_code === 'ja') {
+            wordRegex = /[\S]/g;
+            value = _.replace(s, /[.,\/#!$%\^&\*;:{}=\-_`~()、。「」『』！＠＃＄％＾＆＊（）：”’？＞＜＋＿｜A-Za-z]/g, '');
+        }
+
+        const wordCount = _.words(value, wordRegex).length;
+        this.logger.debug(this, 'wordCount', wordCount);
+        this.emitWordCount.next(wordCount);
     }
 }
