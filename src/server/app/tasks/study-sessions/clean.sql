5a04ad60a32785bcc26a88d4e666aea8a109a3ca
Hank Ehly <henry.ehly@gmail.com>
2017-05-08 21:54:50 +0900

    Remove server code from client project

--- src/server/app/tasks/study-sessions/clean.sql
+++
@@ -1,8 +0,0 @@
-SET @yesterday = DATE_SUB(NOW(), INTERVAL 1 DAY);
-
-DELETE FROM writing_answers WHERE study_session_id IN (
-		SELECT id FROM study_sessions WHERE is_completed = false AND created_at < @yesterday
-);
-
-DELETE FROM study_sessions
-WHERE is_completed = false AND created_at < @yesterday;
--- src/server/app/tasks/verification-tokens/clean.sql
+++
@@ -1,2 +0,0 @@
-DELETE FROM verification_tokens
-WHERE expiration_date <= NOW();
