b4838fd258fbaab7f28f34e17ad5611c9dab24ba
Hank Ehly <henry.ehly@gmail.com>
2017-06-08 13:15:30 +0900

    Reset e2e tests

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,9 @@
+import { GetNativeComPage } from './app.po';
+
+describe('get-native.com/', () => {
+    let page: GetNativeComPage;
+
+    beforeEach(() => {
+        page = new GetNativeComPage();
+    });
+});
