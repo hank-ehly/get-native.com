bb42405d9b231f06c6936f2c7a239d198c9c27e1
Hank Ehly <henry.ehly@gmail.com>
2017-06-08 13:13:34 +0900

    Enable component specs

--- src/app/shared/password-strength/password-strength.component.spec.ts
+++ src/app/shared/password-strength/password-strength.component.spec.ts
@@ -15,40 +15,38 @@ import { Logger } from '../../core/logger/logger';
 import { STUBLogger } from '../../core/logger/logger.stub';
 import { STUBPasswords } from '../../core/spec/stubs';
 
-export function main() {
+describe('PasswordStrengthComponent', () => {
     let comp: PasswordStrengthComponent;
     let fixture: ComponentFixture<PasswordStrengthComponent>;
     let el: HTMLElement;
     let util: SpecUtil;
 
-    describe('PasswordStrengthComponent', () => {
-        beforeEach(async(() => {
-            TestBed.configureTestingModule({
-                declarations: [PasswordStrengthComponent],
-                providers: [
-                    StringService,
-                    PasswordService,
-                    {provide: Logger, useValue: STUBLogger}
-                ]
-            }).compileComponents().then(() => {
-                fixture = TestBed.createComponent(PasswordStrengthComponent);
-                util = new SpecUtil(fixture);
-                comp = fixture.componentInstance;
-                fixture.detectChanges();
-            });
-        }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [PasswordStrengthComponent],
+            providers: [
+                StringService,
+                PasswordService,
+                {provide: Logger, useValue: STUBLogger}
+            ]
+        }).compileComponents().then(() => {
+            fixture = TestBed.createComponent(PasswordStrengthComponent);
+            util = new SpecUtil(fixture);
+            comp = fixture.componentInstance;
+            fixture.detectChanges();
+        });
+    }));
 
-        it('should display a VERY WEAK state color', () => {
-            comp.password = STUBPasswords.veryWeak;
-            fixture.detectChanges();
-            el = util.getNativeEl('.meter__segment--very-weak');
-            expect(el.className).toContain('meter__segment--visible');
-        });
-        it('should display a VERY WEAK state label', () => {
-            comp.password = STUBPasswords.veryWeak;
-            fixture.detectChanges();
-            el = util.getNativeEl('.meter__description');
-            expect(el.textContent).toEqual('VERY WEAK');
-        });
+    it('should display a VERY WEAK state color', () => {
+        comp.password = STUBPasswords.veryWeak;
+        fixture.detectChanges();
+        el = util.getNativeEl('.meter__segment--very-weak');
+        expect(el.className).toContain('meter__segment--visible');
     });
-}
+    it('should display a VERY WEAK state label', () => {
+        comp.password = STUBPasswords.veryWeak;
+        fixture.detectChanges();
+        el = util.getNativeEl('.meter__description');
+        expect(el.textContent).toEqual('VERY WEAK');
+    });
+});
