475b9686214e9e4add4ba4f68cc1ba3fc08b8c54
Hank Ehly <henry.ehly@gmail.com>
2017-07-28 08:01:32 +0900

    Fix compiler errors

--- src/app/study/listening/listening-resolver.service.ts
+++ src/app/study/listening/listening-resolver.service.ts
@@ -22,7 +22,7 @@ export class ListeningResolver implements Resolve<Video> {
     constructor(private http: HttpService, private studySession: StudySessionService) {
     }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<void> {
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<Video> {
         // todo: if you refresh, you still send another request for the video. Couldn't you instead use the cache?
         // consider: you may have previously saved data?
         return this.http.request(APIHandle.VIDEO, {
@@ -31,6 +31,7 @@ export class ListeningResolver implements Resolve<Video> {
             }
         }).map((video: Video) => {
             this.studySession.updateCurrent({video: video});
+            return video;
         }).toPromise();
     }
 }
