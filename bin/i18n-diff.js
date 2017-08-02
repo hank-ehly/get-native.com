160a55c05cfc7064cbcbbcad189de8e4aa567519
Hank Ehly <henry.ehly@gmail.com>
2017-08-02 21:58:56 +0900

    Wrap i18n-diff logic in try catch handler

--- bin/i18n-diff.js
+++ bin/i18n-diff.js
@@ -10,13 +10,17 @@ const hashRegExp = /[0-9A-Za-z]{32,}/g;
 const localesDir = path.resolve(__dirname, '..', 'src', 'locales');
 
 async function diff(a, b) {
-    const aText = await readFile(path.resolve(localesDir, `messages.${a}.xlf`), 'utf8');
-    const bText = await readFile(path.resolve(localesDir, `messages.${b}.xlf`), 'utf8');
+    try {
+        const aText = await readFile(path.resolve(localesDir, `messages.${a}.xlf`), 'utf8');
+        const bText = await readFile(path.resolve(localesDir, `messages.${b}.xlf`), 'utf8');
 
-    const difference = _.difference(aText.match(hashRegExp), bText.match(hashRegExp));
+        const difference = _.difference(aText.match(hashRegExp), bText.match(hashRegExp));
 
-    console.log(`Diff ${a}:${b}`);
-    console.log(util.inspect(difference, null));
+        console.log(`Diff ${a}:${b}`);
+        console.log(util.inspect(difference, null));
+    } catch (e) {
+        console.log(e);
+    }
 }
 
 if (process.argv.length < 4) {
