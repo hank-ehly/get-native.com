d8483065717d61df15707e43463bc229b3d58500
Hank Ehly <henry.ehly@gmail.com>
2017-08-06 10:10:30 +0900

    Add more dynamic translations

--- src/app/shared/password-strength/password-strength.component.ts
+++ src/app/shared/password-strength/password-strength.component.ts
@@ -18,40 +18,12 @@ export class PasswordStrengthComponent implements OnChanges {
     @Input() password: string;
     score = 0;
 
-    matrix: [number, string][] = [
-        [0, 'VERY WEAK'],
-        [20, 'WEAK'],
-        [40, 'GOOD'],
-        [60, 'STRONG'],
-        [80, 'VERY STRONG']
-    ];
-
-    private _strengthLabel: string;
-
     constructor(private passwordService: PasswordService) {
     }
 
     ngOnChanges(changes: SimpleChanges): void {
-        if (!changes['password']) {
-            return;
+        if (changes['password']) {
+            this.score = this.passwordService.calculateStrength(changes['password'].currentValue);
         }
-
-        this.score = this.passwordService.calculateStrength(changes['password'].currentValue);
-    }
-
-    get strengthLabel(): string {
-        this._strengthLabel = this.matrix[0][1];
-
-        for (const row in this.matrix) {
-            if (this.matrix.hasOwnProperty(row)) {
-                const min = this.matrix[row][0];
-                const txt = this.matrix[row][1];
-                if (this.score >= min) {
-                    this._strengthLabel = txt;
-                }
-            }
-        }
-
-        return this._strengthLabel;
     }
 }
