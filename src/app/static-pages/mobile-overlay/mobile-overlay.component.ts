68dd1d7d79f55439b788fafe62ae86afd6c1bc67
Hank Ehly <henry.ehly@gmail.com>
2017-08-07 20:22:10 +0900

    Clean up boilerplate

--- src/app/static-pages/mobile-overlay/mobile-overlay.component.ts
+++ src/app/static-pages/mobile-overlay/mobile-overlay.component.ts
@@ -1,16 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'gn-mobile-overlay',
     templateUrl: './mobile-overlay.component.html',
     styleUrls: ['./mobile-overlay.component.scss']
 })
-export class MobileOverlayComponent implements OnInit {
-
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
-
+export class MobileOverlayComponent {
 }
