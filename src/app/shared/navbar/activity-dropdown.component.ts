02b053dbc2331e75d7b0e5dfc1256f98054711e6
Hank Ehly <henry.ehly@gmail.com>
2017-08-07 21:49:49 +0900

    Rename 'notifications' to 'activity'

---
+++ src/app/shared/navbar/activity-dropdown.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gn-activity-dropdown',
+    templateUrl: './activity-dropdown.component.html',
+    styleUrls: ['./activity-dropdown.component.scss']
+})
+export class ActivityDropdownComponent {
+}
