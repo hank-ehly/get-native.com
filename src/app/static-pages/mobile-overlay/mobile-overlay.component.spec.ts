5cdc8bd018283cb53e53bc40ced3b0370e5decd6
Hank Ehly <henry.ehly@gmail.com>
2017-08-07 20:22:10 +0900

    Clean up boilerplate

--- src/app/static-pages/mobile-overlay/mobile-overlay.component.spec.ts
+++ src/app/static-pages/mobile-overlay/mobile-overlay.component.spec.ts
@@ -3,23 +3,22 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { MobileOverlayComponent } from './mobile-overlay.component';
 
 describe('MobileOverlayComponent', () => {
-  let component: MobileOverlayComponent;
-  let fixture: ComponentFixture<MobileOverlayComponent>;
+    let component: MobileOverlayComponent;
+    let fixture: ComponentFixture<MobileOverlayComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ MobileOverlayComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [MobileOverlayComponent]
+        }).compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(MobileOverlayComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(MobileOverlayComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should be created', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should be created', () => {
+        return expect(component).toBeTruthy();
+    });
 });
