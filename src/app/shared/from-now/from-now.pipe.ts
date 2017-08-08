1520f10e1e47fad4fa8e072b606f5ee98642006f
Hank Ehly <henry.ehly@gmail.com>
2017-08-08 21:29:51 +0900

    Parse date string in fromNow pipe

--- src/app/shared/from-now/from-now.pipe.ts
+++ src/app/shared/from-now/from-now.pipe.ts
@@ -1,12 +1,16 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 import * as moment from 'moment';
+import { UTCDateService } from '../../core/utc-date/utc-date.service';
 
 @Pipe({
     name: 'fromNow'
 })
 export class FromNowPipe implements PipeTransform {
-    transform(value: string | Date | moment.Moment, args?: any): any {
-        return moment(value).fromNow();
+    constructor(private date: UTCDateService) {
+    }
+
+    transform(value: string, args?: any): any {
+        return moment(this.date.parse(value)).fromNow();
     }
 }
