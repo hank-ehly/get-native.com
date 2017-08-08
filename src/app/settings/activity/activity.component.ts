5f231efe41ea992e24592d611e72db80df3d7689
Hank Ehly <henry.ehly@gmail.com>
2017-08-08 14:53:59 +0900

    Add mockup for activity component

--- src/app/settings/activity/activity.component.ts
+++ src/app/settings/activity/activity.component.ts
@@ -1,14 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'gn-activity',
     templateUrl: './activity.component.html',
     styleUrls: ['./activity.component.scss']
 })
-export class ActivityComponent implements OnInit {
-    constructor() {
-    }
-
-    ngOnInit() {
-    }
+export class ActivityComponent {
+    activityItems = [
+        {
+            title: 'System Maintenance Notice',
+            created_at: 'Mon Jul 31 20:22:37 +0900 2017',
+            body: `getnative will be temporarily unavailable on Friday, August 15th from 11:00 AM to 2:00 PM (ETC) for system
+maintenance. We apologize for the inconvenience.`,
+            footer: '{Contact us|help}'
+        }
+    ];
 }
