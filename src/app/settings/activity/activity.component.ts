5b29bd08a7192050b4332c992ebef05a2d7f9aa2
Hank Ehly <henry.ehly@gmail.com>
2017-08-08 21:18:23 +0900

    Set activityItems to '[]' for now

--- src/app/settings/activity/activity.component.ts
+++ src/app/settings/activity/activity.component.ts
@@ -6,13 +6,14 @@ import { Component } from '@angular/core';
     styleUrls: ['./activity.component.scss']
 })
 export class ActivityComponent {
-    activityItems = [
-        {
-            title: 'System Maintenance Notice',
-            created_at: 'Mon Jul 31 20:22:37 +0900 2017',
-            body: `getnative will be temporarily unavailable on Friday, August 15th from 11:00 AM to 2:00 PM (ETC) for system
-maintenance. We apologize for the inconvenience.`,
-            footer: '{Contact us|help}'
-        }
-    ];
+    activityItems = [];
+//     activityItems = [
+//         {
+//             title: 'System Maintenance Notice',
+//             created_at: 'Mon Jul 31 20:22:37 +0900 2017',
+//             body: `getnative will be temporarily unavailable on Friday, August 15th from 11:00 AM to 2:00 PM (ETC) for system
+// maintenance. We apologize for the inconvenience.`,
+//             footer: '{Contact us|help}'
+//         }
+//     ];
 }
