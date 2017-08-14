6a0c69104febb0694fce5f59224906ad837f2254
Hank Ehly <henry.ehly@gmail.com>
2017-08-14 17:15:17 +0900

    Use observables to unsubscribe

--- src/app/study/listening/listening.component.ts
+++ src/app/study/listening/listening.component.ts
@@ -9,39 +9,33 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 
 import { StudySessionService } from '../../core/study-session/study-session.service';
 import { kShadowing } from '../../core/study-session/section-keys';
-import { Transcript } from '../../core/entities/transcript';
-import { Entities } from '../../core/entities/entities';
 import { Logger } from '../../core/logger/logger';
 
-import { Subscription } from 'rxjs/Subscription';
-import 'rxjs/add/operator/pluck';
-import 'rxjs/add/operator/take';
-import * as _ from 'lodash';
+import { Subject } from 'rxjs/Subject';
+import 'rxjs/add/operator/takeUntil';
 
 @Component({
     templateUrl: 'listening.component.html',
     styleUrls: ['listening.component.scss']
 })
 export class ListeningComponent implements OnInit, OnDestroy {
-    transcripts: Entities<Transcript> = this.session.current.video.transcripts;
-    src: string = this.session.current.video.video_url;
 
-    subscriptions: Subscription[] = [];
+    transcripts = this.session.current.video.transcripts;
+    src = this.session.current.video.video_url;
+    OnDestroy$ = new Subject<void>();
 
     constructor(private logger: Logger, private session: StudySessionService) {
     }
 
     ngOnInit(): void {
         this.logger.debug(this, 'OnInit');
-
-        this.subscriptions.push(
-            this.session.progressEmitted$.subscribe(this.session.progress.listeningEmitted$),
-            this.session.progressEmitted$.subscribe(null, null, () => this.session.transition(kShadowing))
-        );
+        this.session.progressEmitted$.takeUntil(this.OnDestroy$).subscribe(this.session.progress.listeningEmitted$);
+        this.session.progressEmitted$.takeUntil(this.OnDestroy$).subscribe(null, null, () => this.session.transition(kShadowing));
     }
 
     ngOnDestroy(): void {
         this.logger.debug(this, 'OnDestroy');
-        _.each(this.subscriptions, s => s.unsubscribe());
+        this.OnDestroy$.next();
     }
+
 }
