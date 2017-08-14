f0287927ac34b0678a0906ec4bede74634916996
Hank Ehly <henry.ehly@gmail.com>
2017-08-14 17:06:02 +0900

    Use cached video if exists in ListeningResolver

--- src/app/study/listening/listening-resolver.service.ts
+++ src/app/study/listening/listening-resolver.service.ts
@@ -11,27 +11,28 @@ import { Injectable } from '@angular/core';
 import { StudySessionService } from '../../core/study-session/study-session.service';
 import { HttpService } from '../../core/http/http.service';
 import { APIHandle } from '../../core/http/api-handle';
+import { Logger } from '../../core/logger/logger';
 import { Video } from '../../core/entities/video';
 
-import 'rxjs/add/operator/toPromise';
-import 'rxjs/add/operator/catch';
+import { Observable } from 'rxjs/Observable';
 import 'rxjs/add/operator/map';
 
 @Injectable()
 export class ListeningResolver implements Resolve<Video> {
-    constructor(private http: HttpService, private studySession: StudySessionService) {
+    constructor(private http: HttpService, private studySession: StudySessionService, private logger: Logger) {
     }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Promise<Video> {
-        // todo: if you refresh, you still send another request for the video. Couldn't you instead use the cache?
-        // consider: you may have previously saved data?
-        return this.http.request(APIHandle.VIDEO, {
-            params: {
-                id: this.studySession.current.session.video_id
-            }
-        }).map((video: Video) => {
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Video> {
+        if (this.studySession.current.video) {
+            this.logger.debug(this, 'using cached study session video:', this.studySession.current.video);
+            return Observable.of(this.studySession.current.video);
+        }
+
+        this.logger.debug(this, 'no cached video present. requesting from API');
+        const options = {params: {id: this.studySession.current.session.video_id}};
+        return this.http.request(APIHandle.VIDEO, options).map((video: Video) => {
             this.studySession.updateCurrent({video: video});
             return video;
-        }).toPromise();
+        });
     }
 }
