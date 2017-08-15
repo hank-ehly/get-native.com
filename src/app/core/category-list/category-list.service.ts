bdb11dba4486fab4fd305f5574126d232b03c829
Hank Ehly <henry.ehly@gmail.com>
2017-08-15 17:38:32 +0900

    Request correct language categories if unauthenticated

--- src/app/core/category-list/category-list.service.ts
+++ src/app/core/category-list/category-list.service.ts
@@ -5,12 +5,16 @@
  * Created by henryehly on 2017/02/05.
  */
 
-import { Injectable } from '@angular/core';
+import { Inject, Injectable, LOCALE_ID } from '@angular/core';
+import { URLSearchParams } from '@angular/http';
 
 import { HttpService } from '../http/http.service';
-import { APIHandle } from '../http/api-handle';
+import { UserService } from '../user/user.service';
+import { LangService } from '../lang/lang.service';
 import { Entities } from '../entities/entities';
 import { Category } from '../entities/category';
+import { APIHandle } from '../http/api-handle';
+import { Entity } from '../entities/entity';
 
 import { ReplaySubject } from 'rxjs/ReplaySubject';
 import { Observable } from 'rxjs/Observable';
@@ -19,12 +23,21 @@ import { Observable } from 'rxjs/Observable';
 export class CategoryListService {
     private data$ = new ReplaySubject<Entities<Category>>(1);
 
-    constructor(private http: HttpService) {
+    constructor(private http: HttpService, private user: UserService, @Inject(LOCALE_ID) private localeId: string,
+                private lang: LangService) {
     }
 
     fetch(): Observable<Entities<Category>> {
         if (!this.data$.observers.length) {
-            this.http.request(APIHandle.CATEGORIES).subscribe(this.onFetchSuccess.bind(this), this.onFetchError.bind(this));
+            let request: Observable<Entities<Entity>|Entity>;
+            if (this.user.isAuthenticated()) {
+                request = this.http.request(APIHandle.CATEGORIES);
+            } else {
+                const searchParams = new URLSearchParams();
+                searchParams.set('lang', this.lang.languageForLocaleId(this.localeId).code);
+                request = this.http.request(APIHandle.CATEGORIES, {search: searchParams});
+            }
+            request.subscribe(this.onFetchSuccess.bind(this), this.onFetchError.bind(this));
         }
 
         return this.data$;
