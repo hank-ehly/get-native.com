3bd6246070f5a25d0094c7241bebbadb9d45c78b
Hank Ehly <henry.ehly@gmail.com>
2017-08-25 07:49:32 +0900

    Add article about resetting password

--- src/app/help/article/article.component.ts
+++ src/app/help/article/article.component.ts
@@ -1,19 +1,16 @@
-import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Component } from '@angular/core';
 
 @Component({
     selector: 'gn-help-article',
     templateUrl: './article.component.html',
     styleUrls: ['./article.component.scss']
 })
-export class HelpArticleComponent implements OnInit {
+export class HelpArticleComponent {
     articleId: number;
 
     constructor(private route: ActivatedRoute) {
-        this.articleId = route.snapshot.params.id;
-    }
-
-    ngOnInit() {
+        this.articleId = route.snapshot.params['id'];
     }
 
 }
--- src/app/help/help.module.ts
+++ src/app/help/help.module.ts
@@ -6,6 +6,7 @@ import { HelpArticleComponent } from './article/article.component';
 import { SharedModule } from '../shared/shared.module';
 import { HelpService } from './help.service';
 import { HelpArticle20170818Component } from './article/20170818/20170818.component';
+import { HelpArticle20170825Component } from './article/20170825/20170825.component';
 
 @NgModule({
     imports: [
@@ -15,7 +16,8 @@ import { HelpArticle20170818Component } from './article/20170818/20170818.compon
         HelpComponent,
         HelpArticleComponent,
         HelpMainComponent,
-        HelpArticle20170818Component
+        HelpArticle20170818Component,
+        HelpArticle20170825Component
     ],
     exports: [
         HelpComponent,
