575623a9aa0424ce9c3c75670e5b3789d545a34a
Hank Ehly <henry.ehly@gmail.com>
2017-08-25 07:49:32 +0900

    Add article about resetting password

---
+++ src/app/help/article/20170825/20170825.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+    selector: 'gn-help-article-20170825',
+    templateUrl: './20170825.component.html'
+})
+export class HelpArticle20170825Component {
+}
