5accbe12cd4a56d2b8ec0751308679ac417b5b82
Hank Ehly <henry.ehly@gmail.com>
2017-08-30 08:00:34 +0900

    Implement send-password-reset-link logic in pre-login modal

--- src/app/login/forgot-password/forgot-password.component.ts
+++ src/app/login/forgot-password/forgot-password.component.ts
@@ -5,29 +5,73 @@
  * Created by henryehly on 2017/04/24.
  */
 
-import { Component } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 
 import { Logger } from '../../core/logger/logger';
 import { EMAIL_REGEX } from '../../core/typings/email-regex';
 import { LoginModalService } from '../../core/login-modal/login-modal.service';
+import { Subject } from 'rxjs/Subject';
+import { HttpService } from '../../core/http/http.service';
+import { APIHandle } from '../../core/http/api-handle';
+import { APIError, APIErrors } from '../../core/http/api-error';
+import * as _ from 'lodash';
 
 @Component({
     selector: 'gn-forgot-password',
     templateUrl: 'forgot-password.component.html',
     styleUrls: ['forgot-password.component.scss']
 })
-export class ForgotPasswordComponent {
-    email = '';
-    emailRe: string = EMAIL_REGEX;
+export class ForgotPasswordComponent implements OnDestroy {
 
-    constructor(private logger: Logger, private modal: LoginModalService) {
+    email = '';
+    submittedEmail = '';
+    emailRegex: string = EMAIL_REGEX;
+    processing = false;
+    error: APIError;
+
+    private OnDestroy$ = new Subject<void>();
+
+    constructor(private logger: Logger, private modal: LoginModalService, private http: HttpService) {
+    }
+
+    ngOnDestroy(): void {
+        this.logger.debug(this, 'OnDestroy');
+        this.OnDestroy$.next();
     }
 
     onSubmit(): void {
         this.logger.debug(this, 'onSubmit');
+
+        const options = {
+            body: {
+                email: this.email
+            }
+        };
+
+        this.error = null;
+        this.processing = true;
+        this.http.request(APIHandle.SEND_PASSWORD_RESET_LINK, options)
+            .takeUntil(this.OnDestroy$)
+            .subscribe(
+                this.onSendPasswordResetLinkNext.bind(this),
+                this.onSendPasswordResetLinkError.bind(this)
+            );
     }
 
     onSetModalView(view: string): void {
         this.modal.setActiveView(view);
     }
+
+    private onSendPasswordResetLinkNext(): void {
+        this.processing = false;
+        this.submittedEmail = this.email;
+    }
+
+    private onSendPasswordResetLinkError(errors: APIErrors): void {
+        this.processing = false;
+        if (errors.length) {
+            this.error = _.first(errors);
+        }
+    }
+
 }
