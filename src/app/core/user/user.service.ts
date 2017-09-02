b1b6cd425cce54a5a2f0eb42c7423e4517305b0e
Hank Ehly <henry.ehly@gmail.com>
2017-09-02 09:33:18 +0900

    Improve general settings error messaging

--- src/app/core/user/user.service.ts
+++ src/app/core/user/user.service.ts
@@ -9,11 +9,11 @@ import { Injectable } from '@angular/core';
 
 import { kCurrentUser, kAuthToken, kAuthTokenExpire, kAcceptLocalStorage, kCurrentStudySession } from '../local-storage/local-storage-keys';
 import { LocalStorageService } from '../local-storage/local-storage.service';
-import { LanguageCode } from '../typings/language-code';
 import { LangService } from '../lang/lang.service';
 import { HttpService } from '../http/http.service';
 import { APIHandle } from '../http/api-handle';
 import { Language } from '../typings/language';
+import { APIErrors } from '../http/api-error';
 import { Logger } from '../logger/logger';
 import { User } from '../entities/user';
 
@@ -37,25 +37,8 @@ export class UserService {
     compliant$            = new BehaviorSubject<boolean>(this.localStorage.getItem(kAcceptLocalStorage) || false);
     logout$               = new Subject<void>();
 
-    // setters
-    defaultStudyLanguage$ = new Subject<Language>();
-    interfaceLanguage$ = new Subject<Language>();
     $setEmailNotificationsEnabled = new Subject<boolean>();
     $setBrowserNotificationsEnabled = new Subject<boolean>();
-    password$ = new Subject<{current: string, replacement: string}>();
-
-    interfaceLanguageEmitted$ = new Subject();
-
-    // todo: Follow this convention of setters/getters
-    // private emitChangeSource = new Subject<any>();
-    // // Observable string streams
-    // changeEmitted$ = this.emitChangeSource.asObservable();
-    // // Service message commands
-    // emitChange(change: any) {
-    //     this.emitChangeSource.next(change);
-    // }
-
-    passwordChange$ = new Subject();
 
     constructor(private lang: LangService, private localStorage: LocalStorageService, private logger: Logger, private http: HttpService) {
         // todo: Don't change the current study language every time you change the default study language
@@ -65,43 +48,23 @@ export class UserService {
 
         this.current$.filter(_.isObject).mapTo(true).subscribe(this.authenticated$);
         this.logout$.mapTo(false).subscribe(this.authenticated$);
-
         this.current$.filter(_.isObject).mapTo(true).subscribe(this.compliant$);
-
-        this.defaultStudyLanguage$.pluck('code').distinctUntilChanged().concatMap((code: LanguageCode) => {
-            return this.http.request(APIHandle.UPDATE_USER, {body: {default_study_language_code: code}});
-        }, (code: LanguageCode) => {
-            this.updateCache({default_study_language: this.lang.languageForCode(code)});
-        }).subscribe();
-
-        this.interfaceLanguage$.pluck('code').distinctUntilChanged().concatMap((code: LanguageCode) => {
-            return this.http.request(APIHandle.UPDATE_USER, {body: {interface_language_code: code}});
-        }, (code: LanguageCode) => {
-            this.updateCache({interface_language: this.lang.languageForCode(code)});
-            this.interfaceLanguageEmitted$.next(code);
-        }).subscribe();
 
         this.$setEmailNotificationsEnabled.distinctUntilChanged().concatMap((value: boolean) => {
             return this.http.request(APIHandle.UPDATE_USER, {body: {email_notifications_enabled: value}});
         }, (value: boolean) => {
             this.updateCache({email_notifications_enabled: value});
-        }).subscribe();
+        }).subscribe(null, (errors: APIErrors) => {
+            // this is the error handler
+        });
 
         this.$setBrowserNotificationsEnabled.distinctUntilChanged().concatMap((value: boolean) => {
             return this.http.request(APIHandle.UPDATE_USER, {body: {browser_notifications_enabled: value}});
         }, (value: boolean) => {
             this.updateCache({browser_notifications_enabled: value});
-        }).subscribe();
-
-        this.password$.mergeMap((passwords) => {
-            this.logger.debug(this, passwords);
-            return this.http.request(APIHandle.EDIT_PASSWORD, {
-                body: {
-                    current_password: passwords.current,
-                    new_password: passwords.replacement
-                }
-            });
-        }).subscribe(this.passwordChange$);
+        }).subscribe(null, (errors: APIErrors) => {
+            // this is the error handler
+        });
     }
 
     updateCache(user: User): void {
@@ -142,14 +105,6 @@ export class UserService {
     update(user: User): void {
         this.logger.debug(this, 'update', user);
 
-        if (_.has(user, 'default_study_language')) {
-            this.defaultStudyLanguage$.next(user.default_study_language);
-        }
-
-        if (_.has(user, 'interface_language')) {
-            this.interfaceLanguage$.next(user.interface_language);
-        }
-
         if (_.has(user, 'email_notifications_enabled')) {
             this.$setEmailNotificationsEnabled.next(user.email_notifications_enabled);
         }
@@ -157,11 +112,6 @@ export class UserService {
         if (_.has(user, 'browser_notifications_enabled')) {
             this.$setBrowserNotificationsEnabled.next(user.browser_notifications_enabled);
         }
-    }
-
-    updatePassword(current: string, replacement: string) {
-        this.logger.debug(this, 'updatePassword');
-        this.password$.next({current: current, replacement: replacement});
     }
 
     isAuthenticated(): boolean {
