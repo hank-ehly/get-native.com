339ef57d2717d35cea8f90eef2e98368fa256d27
Hank Ehly <henry.ehly@gmail.com>
2017-09-07 08:36:29 +0900

    Display image cropper

--- src/app/settings/settings.component.ts
+++ src/app/settings/settings.component.ts
@@ -5,12 +5,15 @@
  * Created by henryehly on 2016/12/09.
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 
 import { Logger } from '../core/logger/logger';
 
+import { CropperSettings, ImageCropperComponent } from 'ng2-img-cropper';
 import * as _ from 'lodash';
+import { User } from '../core/entities/user';
+import { UserService } from '../core/user/user.service';
 
 @Component({
     selector: 'gn-settings',
@@ -18,14 +21,77 @@ import * as _ from 'lodash';
     styleUrls: ['settings.component.scss']
 })
 export class SettingsComponent implements OnInit {
+
+    @ViewChild(ImageCropperComponent) cropper: ImageCropperComponent;
+    data: any = {};
+    image: any;
+    isCropperModalVisible = false;
+    isThumbnailDropdownVisible = false;
     selectedTab: string;
 
-    constructor(private logger: Logger, private router: Router) {
+    // half of main width - half of dropdown width
+    dropdownLeft = 300 - (232 / 2) + 'px';
+    dropdownTop = '80px';
+
+    cropperSettings: CropperSettings = _.assign(new CropperSettings(), {
+        width: 110,
+        height: 110,
+        croppedWidth: 110,
+        croppedHeight: 110,
+        minWidth: 110,
+        minHeight: 110,
+        canvasWidth: 400,
+        canvasHeight: 300,
+        cropperDrawSettings: {
+            strokeWidth: 2,
+            strokeColor: '#FFFFFF',
+            dragIconStrokeWidth: 1,
+            dragIconStrokeColor: '#FFFFFF',
+            dragIconFillColor: '#FFFFFF'
+        },
+        rounded: false,
+        noFileInput: true,
+        keepAspect: true,
+        cropperClass: 'canvas',
+        croppingClass: 'canvas--populated'
+    });
+
+    user: User = this.userService.current$.getValue();
+
+    constructor(private logger: Logger, private router: Router, private userService: UserService) {
     }
 
     ngOnInit() {
         this.logger.debug(this, 'ngOnInit');
         this.selectedTab = this.getSelectedTab();
+    }
+
+    onClickThumbnail(): void {
+        this.isThumbnailDropdownVisible = !this.isThumbnailDropdownVisible;
+    }
+
+    onChangeThumbnail(e: Event): void {
+        this.isThumbnailDropdownVisible = false;
+        this.isCropperModalVisible = true;
+        this.image = new Image();
+        const file: File = _.first((<HTMLInputElement>e.target).files);
+        const reader = new FileReader();
+        reader.onloadend = this.onLoadEnd.bind(this);
+        reader.readAsDataURL(file);
+    }
+
+    onClickUploadPhoto(): void {
+        //
+    }
+
+    onClickRemovePhoto(): void {
+        if (this.user.is_silhouette_picture) {
+            this.isThumbnailDropdownVisible = false;
+        }
+    }
+
+    onClickCloseDropdown(): void {
+        this.isThumbnailDropdownVisible = false;
     }
 
     setSelectedTab(tab: string) {
@@ -47,5 +113,19 @@ export class SettingsComponent implements OnInit {
         }
 
         return tabValue;
+    }
+
+    onCancelCrop(): void {
+        this.cropper.reset();
+        this.isCropperModalVisible = false;
+    }
+
+    onApplyCrop(): void {
+        this.isCropperModalVisible = false;
+    }
+
+    private onLoadEnd(e: any): void {
+        this.image.src = e.target.result;
+        this.cropper.setImage(this.image);
     }
 }
