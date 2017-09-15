55f466e9859bce71ca14d7c1098a7c32af686742
Hank Ehly <henry.ehly@gmail.com>
2017-09-15 15:00:06 +0900

    Remove unused import from dashboard resolve service

--- src/app/dashboard/dashboard-resolve.service.ts
+++ src/app/dashboard/dashboard-resolve.service.ts
@@ -1,39 +1,35 @@
-import { ActivatedRouteSnapshot, Resolve, Router, RouterStateSnapshot } from '@angular/router';
+import { ActivatedRouteSnapshot, Resolve, RouterStateSnapshot } from '@angular/router';
 import { Injectable } from '@angular/core';
 
 import { kAuthToken, kAuthTokenExpire } from '../core/local-storage/local-storage-keys';
 import { LocalStorageService } from '../core/local-storage/local-storage.service';
 import { UserService } from '../core/user/user.service';
 import { HttpService } from '../core/http/http.service';
-import { Entities } from '../core/entities/entities';
 import { APIHandle } from '../core/http/api-handle';
-import { Entity } from '../core/entities/entity';
 import { Logger } from '../core/logger/logger';
 import { User } from '../core/entities/user';
 
 import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/operator/do';
+import 'rxjs/add/operator/map';
 import * as _ from 'lodash';
 
 @Injectable()
-export class DashboardResolveService implements Resolve<Entities<Entity> | Entity> {
+export class DashboardResolveService implements Resolve<void> {
     constructor(private localStorage: LocalStorageService, private logger: Logger,
-                private http: HttpService, private user: UserService, private router: Router) {
+                private http: HttpService, private user: UserService) {
     }
 
-    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<Entities<Entity> | Entity> {
+    resolve(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<void> {
         this.logger.debug(this, 'resolve', route, state);
         const expectedKeys = ['token', 'expires'];
         const queryParams = <any>_.pick(route.queryParams, expectedKeys);
         if (_.size(queryParams) === expectedKeys.length && _.every(queryParams, _.isString)) {
             this.localStorage.setItem(kAuthToken, route.queryParams['token']);
             this.localStorage.setItem(kAuthTokenExpire, route.queryParams['expires']);
-            return this.http.request(APIHandle.ME).do((user: User) => {
+            return this.http.request(APIHandle.ME).map((user: User) => {
                 this.user.updateCache(user);
-                // this.router.navigate(['/dashboard']);
             });
         }
-        // this.router.navigate(['/dashboard']);
-        return Observable.of({});
+        return Observable.of(null);
     }
 }
