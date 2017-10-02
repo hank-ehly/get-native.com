2096bbaf9ec5607b9e9124601da1d4305ad887de
Hank Ehly <henry.ehly@gmail.com>
2017-10-02 14:18:16 +0900

    Refactor social login links into buttons

--- src/app/login/social-login/social-login.component.ts
+++ src/app/login/social-login/social-login.component.ts
@@ -19,10 +19,41 @@ export class SocialLoginComponent {
 
     e = environment;
 
+    providers = {
+        FACEBOOK: 0,
+        TWITTER: 1,
+        GOOGLE: 2
+    };
+
+    flags = {
+        processing: {
+            navigation: false
+        }
+    };
+
     constructor(private loginModal: LoginModalService) {
     }
 
     onSetModalView(view: string): void {
         this.loginModal.setActiveView(view);
     }
+
+    onClickSocialButton(provider: number): void {
+        this.flags.processing.navigation = true;
+        window.location.href = this.OAuthURLForProvider(provider);
+    }
+
+    private OAuthURLForProvider(provider: number): string {
+        switch (provider) {
+            case this.providers.FACEBOOK:
+                return this.e.facebookLoginUrl;
+            case this.providers.TWITTER:
+                return this.e.twitterLoginUrl;
+            case this.providers.GOOGLE:
+                return this.e.googleLoginUrl;
+            default:
+                throw new Error(`provider ${provider} is not a valid provider.`);
+        }
+    }
+
 }
