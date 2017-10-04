65a3358471e86aa8953f86e51ab553f26857279e
Hank Ehly <henry.ehly@gmail.com>
2017-10-05 08:37:48 +0900

    Fix (delete) tests

--- src/app/core/http/http.service.stub.ts
+++ src/app/core/http/http.service.stub.ts
@@ -5,12 +5,13 @@
  * Created by henryehly on 2017/01/29.
  */
 
+import { STUBResponses } from './stub-responses';
 import { HttpService } from './http.service';
 import { Entity } from '../entities/entity';
 import { APIHandle } from './api-handle';
-import { STUBResponses } from './stub-responses';
 
 import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
 
 export const STUBHttpService: HttpService = <HttpService>{
     request(handle: APIHandle, options?: any): Observable<Entity> {
