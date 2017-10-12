1f1e50cb18af580abf26344e96c3bedd3cba6b38
Hank Ehly <henry.ehly@gmail.com>
2017-10-12 13:37:49 -0500

    Delete google-storage-image directive spec

--- src/app/shared/google-storage-image/google-storage-image.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { GoogleStorageImageDirective } from './google-storage-image.directive';
-
-describe('GoogleStorageImageDirective', () => {
-  it('should create an instance', () => {
-    const directive = new GoogleStorageImageDirective();
-    expect(directive).toBeTruthy();
-  });
-});
