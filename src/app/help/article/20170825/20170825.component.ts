5a89d3c72ace6fb28c6dff462a220f691dadbc54
Hank Ehly <henry.ehly@gmail.com>
2017-10-16 08:06:54 +0900

    Update japanese translations

--- src/app/help/article/20170825/20170825.component.ts
+++ src/app/help/article/20170825/20170825.component.ts
@@ -3,19 +3,7 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'gn-help-article-20170825',
     templateUrl: './20170825.component.html',
-    styles: [`
-        .img-1 {
-            display: block;
-            max-width: 700px;
-            margin: 30px auto 120px;
-        }
-
-        .img-2 {
-            display: block;
-            width: 400px;
-            margin: 30px auto;
-        }
-    `]
+    styleUrls: ['./20170825.component.scss']
 })
 export class HelpArticle20170825Component {
 }
