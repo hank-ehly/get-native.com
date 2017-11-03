67b41b38e18c31657425a5c8ae8dd04b9bcc4e67
Hank Ehly <henry.ehly@gmail.com>
2017-11-03 10:22:21 +0900

    Try using rsync to send pre-built files

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -6,8 +6,21 @@ if [ -z ${1} ]; then
 fi
 
 if [ "${1}" == "develop" ]; then
-	git clone git@github.com:hank-ehly/devops.getnativelearning.com.git
-	cd devops.getnativelearning.com/deploy && bundle install --jobs 4 --path vendor/bundle && bundle exec cap client:staging deploy
+	for locale in en ja; do
+		npm run build -- \
+			--env stg \
+			--aot \
+			--output-hashing=all \
+			--sourcemaps=false \
+			--extract-css=true \
+			--output-path=dist/${locale} \
+			--bh "/${locale}/" \
+			--i18n-file=src/locales/messages.${locale}.xlf \
+			--i18n-format=xlf \
+			--locale=${locale} \
+			--progress=false
+	done
+	rsync -avz dist getnative@45.79.159.54:/var/www/getnativelearning.com/current/dist
 elif [ "${1}" == "master" ]; then
 	git clone git@github.com:hank-ehly/devops.getnativelearning.com.git
 	cd devops.getnativelearning.com/deploy && bundle install --jobs 4 --path vendor/bundle && bundle exec cap client:production deploy
