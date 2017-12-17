218fb5e8b317000d58adbeadc22b5c9fdd1cbafb
Hank Ehly <henry.ehly@gmail.com>
2017-12-17 20:48:10 +0900

    Use snake case for localStorage keys

--- src/app/core/local-storage/local-storage-keys.ts
+++ src/app/core/local-storage/local-storage-keys.ts
@@ -7,10 +7,9 @@
 
 import { environment } from '../../../environments/environment';
 
-export const kAcceptLocalStorage   = environment.production ? 'FCjKY3' : 'accept-local-storage'  ;
-export const kDebugModalPosition   = environment.production ? 'x2UeqU' : 'debug-modal-position'  ;
-export const kAuthToken            = environment.production ? '2C9q5J' : 'auth-token'            ;
-export const kAuthTokenExpire      = environment.production ? 'Jj35Bv' : 'auth-token-expire'     ;
-export const kCurrentUser          = environment.production ? 'QLSxY4' : 'current-user'          ;
-export const kCurrentStudySession  = environment.production ? '6Hj0pQ' : 'current-study-session' ;
-export const kCurrentStudyLanguage = environment.production ? 'p3bkA0' : 'current-study-language';
+export const kAcceptLocalStorage = environment.production ? 'FCjKY3' : 'accept_local_storage';
+export const kAuthToken = environment.production ? '2C9q5J' : 'auth_token';
+export const kAuthTokenExpire = environment.production ? 'Jj35Bv' : 'auth_token_expire';
+export const kCurrentUser = environment.production ? 'QLSxY4' : 'current_user';
+export const kCurrentStudySession = environment.production ? '6Hj0pQ' : 'current_study_session';
+export const kCurrentStudyLanguage = environment.production ? 'p3bkA0' : 'current_study_language';
