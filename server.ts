7490f68811623ffc734faad203908bde908278a6
Hank Ehly <henry.ehly@gmail.com>
2018-02-25 18:37:44 +0900

    Use different locale setup

--- server.ts
+++ server.ts
@@ -9,54 +9,55 @@ import 'zone.js/dist/zone-node';
 import 'reflect-metadata';
 
 import { renderModuleFactory } from '@angular/platform-server';
+import { ngExpressEngine } from '@nguniversal/express-engine';
 import { enableProdMode } from '@angular/core';
 
 import * as express from 'express';
-import * as locale from 'locale';
 import { join } from 'path';
 import { readFileSync } from 'fs';
+import * as locale from 'locale';
+import * as _ from 'lodash';
+
+const {provideModuleMap} = require('@nguniversal/module-map-ngfactory-loader');
 
 enableProdMode();
-
-const app = express();
-app.use(locale(['en', 'en_US', 'ja'], 'en'));
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
 
-const englishTemplate = readFileSync(join(DIST_FOLDER, 'browser', 'en', 'index.html')).toString();
-const japaneseTemplate = readFileSync(join(DIST_FOLDER, 'browser', 'ja', 'index.html')).toString();
+const LOCALES = [
+    {
+        id: 'en',
+        engine: ngExpressEngine({
+            bootstrap: require('main.server.en').AppServerModuleNgFactory,
+            providers: [provideModuleMap(require('main.server.en').LAZY_MODULE_MAP)]
+        })
+    },
+    {
+        id: 'ja',
+        engine: ngExpressEngine({
+            bootstrap: require('main.server.ja').AppServerModuleNgFactory,
+            providers: [provideModuleMap(require('main.server.ja').LAZY_MODULE_MAP)]
+        })
+    }
+];
 
-function templateForLocale(lc) {
-    return lc === 'ja' ? japaneseTemplate : englishTemplate;
-}
-
-const {AppServerModuleNgFactory, LAZY_MODULE_MAP} = require('./dist/server/main.bundle');
-const {provideModuleMap} = require('@nguniversal/module-map-ngfactory-loader');
-
-app.engine('html', (_, options, callback) => {
-    renderModuleFactory(AppServerModuleNgFactory, {
-        document: templateForLocale(options.req.locale),
-        url: options.req.url,
-        extraProviders: [
-            provideModuleMap(LAZY_MODULE_MAP)
-        ]
-    }).then(html => {
-        callback(null, html);
-    });
-});
+const app = express();
+app.use(locale([_.map(LOCALES, 'id')], 'en'));
 
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
-// Server static files from /browser
-app.get('*.*', express.static(join(DIST_FOLDER, 'browser')));
+LOCALES.forEach(lc => {
+    app.get(`/${lc.id}(/*)?`, (req, res) => {
+        res.render(join(DIST_FOLDER, 'browser', lc.id, 'index.html'), {req, res, engine: lc.engine});
+    });
+});
 
-// All regular routes use the Universal engine
 app.get('*', (req, res) => {
-    res.render(join(DIST_FOLDER, 'browser', 'index.html'), {req});
+    res.redirect([req.locale, req.url].join('').replace(/\/+/g, '/'));
 });
 
 app.listen(PORT, () => {
-    console.log(`Node server listening on http://localhost:${PORT}`);
+    console.log(`listening on http://localhost:${PORT}`);
 });
