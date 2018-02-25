6aed680c027c4d1ecc5f26961b2ecbdea012800d
Hank Ehly <henry.ehly@gmail.com>
2018-02-25 20:02:18 +0900

    Update locales array

--- server.ts
+++ server.ts
@@ -13,6 +13,8 @@ import { enableProdMode } from '@angular/core';
 
 import * as express from 'express';
 import { join } from 'path';
+import * as _ from 'lodash';
+
 const locale = require('locale');
 
 const {provideModuleMap} = require('@nguniversal/module-map-ngfactory-loader');
@@ -21,23 +23,26 @@ enableProdMode();
 
 const PORT = process.env.PORT || 4000;
 const DIST_FOLDER = join(process.cwd(), 'dist');
-const LOCALES = ['en', 'ja'];
-
-const engine = ngExpressEngine({
-    bootstrap: require('./dist/server/main.bundle').AppServerModuleNgFactory,
-    providers: [provideModuleMap(require('./dist/server/main.bundle').LAZY_MODULE_MAP)]
+const LOCALES = ['en', 'ja'].map(loc => {
+    return {
+        id: loc,
+        engine: ngExpressEngine({
+            bootstrap: require(`./dist/server/${loc}/main.bundle`).AppServerModuleNgFactory,
+            providers: [provideModuleMap(require(`./dist/server/${loc}/main.bundle`).LAZY_MODULE_MAP)]
+        })
+    };
 });
 
 const app = express();
-app.use(locale(LOCALES, 'en'));
+app.use(locale(_.map(LOCALES, 'id'), 'en'));
 
 app.set('view engine', 'html');
 app.set('views', join(DIST_FOLDER, 'browser'));
 
 LOCALES.forEach(loc => {
-    app.get(`/${loc}(/*)?`, (req, res) => {
-        res.render(join(DIST_FOLDER, 'browser', loc, 'index.html'), {
-            req, res, engine: engine
+    app.get(`/${loc.id}(/*)?`, (req, res) => {
+        res.render(join(DIST_FOLDER, 'browser', loc.id, 'index.html'), {
+            req, res, engine: loc.engine
         });
     });
 });
